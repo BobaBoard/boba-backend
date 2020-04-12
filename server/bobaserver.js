16140c238da298f81bf6f587292785939790a3d4
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-04-12 15:38:48 -0700

    add .env file

--- server/bobaserver.js
+++ server/bobaserver.js
@@ -1,6 +1,8 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
