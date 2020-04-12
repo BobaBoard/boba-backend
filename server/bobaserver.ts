ee9eb3f3fc55fb0cad8ba030a1e74c99fe4d24c4
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-04-12 16:05:06 -0700

    add typescript

--- server/bobaserver.ts
+++ server/bobaserver.ts
@@ -1,6 +1,7 @@
-const express = require("express");
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
