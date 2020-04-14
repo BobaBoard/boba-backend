011e8c384302a0977f300c2f3c05d280e8c98a41
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-04-14 15:59:16 -0700

    add mocha tests for board API

--- server/bobaserver.ts
+++ server/bobaserver.ts
@@ -9,12 +9,22 @@ const port = process.env.PORT || 3000;
 const databaseConfig = { connectionString: process.env.DATABASE_URL };
 const pool = new Pool(databaseConfig);
 
-app.get("/", async (req, res) => {
-  const {
-    rows,
-  } = await pool.query(
-    "SELECT * FROM Threads LEFT JOIN Boards ON Threads.parentBoard = Boards.id WHERE Boards.stringId=$1",
-    ["gore"]
+app.get("/boards", async (req, res) => {
+  const boardId = req.query.boardId;
+
+  const { rows } = await pool.query(
+    `SELECT 
+      Boards.title as boardTitle,
+      Boards.description as boardDescription,
+      Boards.avatar as boardAvatar,
+      Boards.stringId as boardId,
+      Threads.content as threadContent,
+      Users.name as threadAuthor
+     FROM Threads 
+      LEFT JOIN Boards ON Threads.parentBoard = Boards.id 
+      LEFT JOIN Users ON Threads.author = Users.id 
+     WHERE Boards.stringId=$1`,
+    [boardId]
   );
   res.send(rows);
 });
--- test/bobaserver.test.ts
+++ test/bobaserver.test.ts
@@ -6,18 +6,18 @@ import app from "../server/bobaserver";
 
 describe("Tests boards REST API", () => {
   it("should return board data", async () => {
-    const res = await request(app).get("/").send();
+    const res = await request(app).get("/boards").query({
+      boardId: "gore",
+    });
+
     expect(res.status).to.equal(200);
     expect(res.body[0]).to.eql({
-      author: "3",
-      avatar: null,
-      content: "Hi this is  a second thread",
-      description: "Everything the light touches is dead doves.",
-      id: "2",
-      parentboard: "2",
-      settings: {},
-      stringid: "gore",
-      title: "Gore Central",
+      boardtitle: "Gore Central",
+      boarddescription: "Everything the light touches is dead doves.",
+      boardavatar: null,
+      boardid: "gore",
+      threadcontent: "Hi this is  a second thread",
+      threadauthor: "oncest5evah",
     });
   });
 });
