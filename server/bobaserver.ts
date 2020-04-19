7626d850c6fec99c0f45f4787d7f4f0d36d0bf79
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-04-19 15:52:53 -0700

    add docker setup take 8

--- server/bobaserver.ts
+++ server/bobaserver.ts
@@ -22,21 +22,25 @@ app.get("/boards", async (req, res) => {
 
   log(`Fetching data for board with id ${boardId}`);
 
-  const { rows } = await pool.query(
-    `SELECT
-      Boards.title as boardTitle,
-      Boards.description as boardDescription,
-      Boards.avatar as boardAvatar,
-      Boards.stringId as boardId,
-      Threads.content as threadContent,
-      Users.name as threadAuthor
-     FROM Threads
-      LEFT JOIN Boards ON Threads.parentBoard = Boards.id
-      LEFT JOIN Users ON Threads.author = Users.id
-     WHERE Boards.stringId=$1`,
-    [boardId]
-  );
-  res.send(rows);
+  try {
+    const { rows } = await pool.query(
+      `SELECT
+        Boards.title as boardTitle,
+        Boards.description as boardDescription,
+        Boards.avatar as boardAvatar,
+        Boards.stringId as boardId,
+        Threads.content as threadContent,
+        Users.name as threadAuthor
+      FROM Threads
+        LEFT JOIN Boards ON Threads.parentBoard = Boards.id
+        LEFT JOIN Users ON Threads.author = Users.id
+      WHERE Boards.stringId=$1`,
+      [boardId]
+    );
+    res.send(rows);
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
 if (require.main === module) {
