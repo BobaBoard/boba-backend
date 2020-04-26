033e39906462976c0596730b811f4c4ca2e871c3
essential-randomness <essentialrandomn3ss@gmail.com>
2020-04-25 19:39:08 -0700

    Finalize schema (#1)
    * Second pass at schema. Still lot to do.
    
    * more db updates
    
    * finalize thread table
    
    * finalize posts table
    
    * finalize comments table
    
    * add invite types
    
    * fix tags blacklist
    
    * update schema after comments
    
    * resolve comments on db schema
    
    * make new insert queries for db testing
    
    * make tests pass again
    
    * address more comments
    
    * update er diagram (and do one last edit to comments table)

--- server/bobaserver.ts
+++ server/bobaserver.ts
@@ -24,21 +24,34 @@ app.get("/boards", async (req, res) => {
 
   try {
     const { rows } = await pool.query(
-      `SELECT
-        Boards.title as boardTitle,
-        Boards.description as boardDescription,
-        Boards.avatar as boardAvatar,
-        Boards.stringId as boardId,
-        Threads.content as threadContent,
-        Users.name as threadAuthor
-      FROM Threads
-        LEFT JOIN Boards ON Threads.parentBoard = Boards.id
-        LEFT JOIN Users ON Threads.author = Users.id
-      WHERE Boards.stringId=$1`,
+      `
+       WITH board_threads AS
+         (SELECT 
+            Threads.id as threads_id, 
+            Boards.title as boards_title,
+            Threads.title as threads_title, 
+            * 
+          FROM Threads 
+          LEFT JOIN Boards ON Threads.parent_board = Boards.id 
+          WHERE Boards.slug=$1)
+       SELECT DISTINCT ON (Posts.parent_thread)
+        bt.boards_title as boardTitle,
+        bt.description as boardDescription,
+        bt.avatar_reference_id as boardAvatar,
+        bt.slug as boardId,
+        bt.threads_title as threadTitle,
+        Posts.content as threadContent,
+        Users.username as threadAuthor
+      FROM board_threads AS bt
+        LEFT JOIN Posts ON Posts.parent_thread = bt.threads_id
+        LEFT JOIN Users ON Posts.author = Users.id
+      ORDER BY Posts.parent_thread, Posts.created ASC`,
       [boardId]
     );
     res.send(rows);
   } catch (e) {
+    log(`Error while fetching data for ${boardId}.`);
+    log(e);
     res.sendStatus(500);
   }
 });
