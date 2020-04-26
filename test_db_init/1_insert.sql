e1f652488e27953136b3ff0e3a8ae735814670da
essential-randomness <essentialrandomn3ss@gmail.com>
2020-04-25 19:39:08 -0700

    Finalize schema (#1)
    * Second pass at schema. Still lot to do.
    
    * more db updates
    
    * finalize thread table
    
    * finalize posts table
    
    * finalize comments table
    
    * add invite types
    
    * fix tags blacklist
    
    * update schema after comments
    
    * resolve comments on db schema
    
    * make new insert queries for db testing
    
    * make tests pass again
    
    * address more comments
    
    * update er diagram (and do one last edit to comments table)

--- test_db_init/1_insert.sql
+++ test_db_init/1_insert.sql
@@ -1,22 +1,54 @@
-INSERT INTO Boards(title, stringId, description, avatar, settings)
-VALUES
-    ('Main Street, Bobaland.', 'main_street', 'The main board. Keep it safe.', NULL, '{}'),
-    ('Gore Central', 'gore', 'Everything the light touches is dead doves.', NULL, '{}'),
-    ('Videogames', 'vg', 'Random videogame discussions. Tag your NSFW.', NULL, '{}');
-  
-INSERT INTO Users(name)
-VALUES
-    ('bobatan'),
-    ('jersey_devil_69'),
-    ('oncest5evah');
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-INSERT INTO Threads(parentBoard, title, content, author)
+INSERT INTO Boards(title, slug, description, settings)
 VALUES
-  ((SELECT id FROM Boards WHERE stringId = 'main_street'), 
-  'A random thread', 
-  'Hi this is  a thread', 
-  (SELECT id FROM Users WHERE name = 'bobatan')),
-  ((SELECT id FROM Boards WHERE stringId = 'gore'), 
-  'This is another thread', 
-  'Hi this is  a second thread', 
-  (SELECT id FROM Users WHERE name = 'oncest5evah'));
+    ('Main Street, Bobaland.', 'main_street', 'The main board. Keep it safe.', '{}'),
+    ('Gore Central', 'gore', 'Everything the light touches is dead doves.', '{}'),
+    ('Videogames', 'vg', 'Random videogame discussions. Tag your NSFW.', '{}');
+  
+INSERT INTO Users(firebase_id, username, invited_by)
+VALUES
+    ('fb1', 'bobatan', NULL);
+
+INSERT INTO Users(firebase_id, username, invited_by)
+VALUES
+    ('fb2', 'jersey_devil_69', (SELECT id FROM users WHERE username = 'bobatan')),
+    ('fb3', 'oncest5evah', (SELECT id FROM users WHERE username = 'bobatan'));
+
+
+INSERT INTO secret_identities(display_name)
+VALUES
+    ('Sunglasses Raccoon'), 
+    ('Solid Ocelot'), 
+    ('Evil Moth');
+
+WITH
+  new_thread_id AS
+    (INSERT INTO threads(parent_board,title) VALUES ((SELECT id FROM boards WHERE slug = 'gore'), 'Favorite character to maim?') RETURNING id),
+  posts_insert AS 
+    (INSERT INTO posts(string_id, parent_thread, author, content, type, whisper_tags, anonymity_type, created)
+      VALUES
+        (uuid_generate_v4(), 
+        (SELECT id FROM new_thread_id),
+        (SELECT id FROM Users WHERE username = 'bobatan'),
+        '[{"insert":"Revolver Ocelot"}]', 
+        'text', 
+        ARRAY['fight me on this'], 
+        'strangers',
+        now()),
+        (uuid_generate_v4(), 
+        (SELECT id FROM new_thread_id),
+        (SELECT id FROM Users WHERE username = 'oncest5evah'),
+        '[{"insert":"Kermit the Frog"}]', 
+        'text', 
+        ARRAY['Im too ashamed to admit this ok', 'sorry mom', 'YOU WILL NEVER KNOW WHO I AM'], 
+        'everyone',
+        now() + INTERVAL'5 minute'))
+INSERT INTO user_thread_identities(thread_id, user_id, identity_id)
+    VALUES
+     ((SELECT id FROM new_thread_id),
+      (SELECT id FROM Users WHERE username = 'bobatan'),
+      (SELECT id FROM secret_identities WHERE display_name = 'Sunglasses Raccoon')),
+     ((SELECT id FROM new_thread_id),
+      (SELECT id FROM Users WHERE username = 'oncest5evah'),
+      (SELECT id FROM secret_identities WHERE display_name = 'Evil Moth'));
