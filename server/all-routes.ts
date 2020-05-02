ee5701683f40d93a860123ff7def5fa03b8a732b
essential-randomness <essentialrandomn3ss@gmail.com>
2020-05-01 20:59:59 -0700

    Add thread routes (#6)
    * add query for all boards
    
    * update test db to make thread queries more interesting
    
    * add queries by thread id
    
    * add tests for thread queries'
    
    * add every folder to watch
    
    * start adding routes for threads
    
    * add todo for identity management
    
    * make db ids and timestamps stable for testing
    
    * add documentation

--- server/all-routes.ts
+++ server/all-routes.ts
@@ -1,11 +1,13 @@
 import debug from "debug";
 import { Router } from "express";
 import BoardRoutes from "./boards/routes";
+import ThreadRoutes from "./threads/routes";
 
 const log = debug("bobaserver:all-routes");
 
 const ROUTES: { [key: string]: Router } = {
   boards: BoardRoutes,
+  threads: ThreadRoutes,
 };
 
 export const applyRoutes = (router: Router) => {
