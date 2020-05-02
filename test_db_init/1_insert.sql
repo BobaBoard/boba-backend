45783094730e32ce1789421af5ec8c2e27d8e3a8
essential-randomness <essentialrandomn3ss@gmail.com>
2020-05-01 20:59:59 -0700

    Add thread routes (#6)
    * add query for all boards
    
    * update test db to make thread queries more interesting
    
    * add queries by thread id
    
    * add tests for thread queries'
    
    * add every folder to watch
    
    * start adding routes for threads
    
    * add todo for identity management
    
    * make db ids and timestamps stable for testing
    
    * add documentation

--- test_db_init/1_insert.sql
+++ test_db_init/1_insert.sql
@@ -24,28 +24,50 @@ VALUES
 
 WITH
   new_thread_id AS
-    (INSERT INTO threads(parent_board,title) VALUES (
-      (SELECT id FROM boards WHERE slug = 'gore'), 
-       'Favorite character to maim?')
-     RETURNING id),  posts_insert AS 
+    (INSERT INTO threads(string_id,parent_board,title)
+      VALUES (
+        '29d1b2da-3289-454a-9089-2ed47db4967b',
+        (SELECT id FROM boards WHERE slug = 'gore'), 
+        'Favorite character to maim?')
+      RETURNING id),
+  posts_insert AS 
     (INSERT INTO posts(string_id, parent_thread, author, content, type, whisper_tags, anonymity_type, created)
       VALUES
-        (uuid_generate_v4(), 
+        ('619adf62-833f-4bea-b591-03e807338a8e', 
         (SELECT id FROM new_thread_id),
         (SELECT id FROM Users WHERE username = 'bobatan'),
         '[{"insert":"Revolver Ocelot"}]', 
         'text', 
         ARRAY['fight me on this'], 
         'strangers',
-        now()),
-        (uuid_generate_v4(), 
+        to_timestamp('2020-04-30 05:42:00', 'YYYY-MM-DD HH:MI:SS')),
+        ('b95bb260-eae0-456c-a5d0-8ae9e52608d8', 
         (SELECT id FROM new_thread_id),
         (SELECT id FROM Users WHERE username = 'oncest5evah'),
         '[{"insert":"Kermit the Frog"}]', 
         'text', 
         ARRAY['Im too ashamed to admit this ok', 'sorry mom', 'YOU WILL NEVER KNOW WHO I AM'], 
         'everyone',
-        now() + INTERVAL'5 minute'))
+        to_timestamp('2020-04-30 05:42:00', 'YYYY-MM-DD HH:MI:SS') + INTERVAL'5 minute')
+      RETURNING id),
+  comments_insert AS
+    (INSERT INTO comments(string_id, parent_post, author, created, content, anonymity_type)
+      VALUES (
+        '46a16199-33d1-48c2-bb79-4d4095014688',
+        (SELECT id FROM posts_insert ORDER BY id DESC LIMIT 1),
+        (SELECT id FROM Users WHERE username = 'bobatan'),
+        to_timestamp('2020-04-30 05:42:00', 'YYYY-MM-DD HH:MI:SS') + INTERVAL'10 minute',
+        '[{"insert":"OMG ME TOO"}]', 
+        'strangers'
+      ),
+      (
+        '89fc3682-cb74-43f9-9a63-bd97d0f59bb9',
+        (SELECT id FROM posts_insert ORDER BY id DESC LIMIT 1),
+        (SELECT id FROM Users WHERE username = 'bobatan'),
+        to_timestamp('2020-04-30 05:42:00', 'YYYY-MM-DD HH:MI:SS') + INTERVAL'10 minute',
+        '[{"insert":"friends!!!!!"}]', 
+        'strangers'
+      ))
 INSERT INTO user_thread_identities(thread_id, user_id, identity_id)
     VALUES
      ((SELECT id FROM new_thread_id),
@@ -57,29 +79,31 @@ INSERT INTO user_thread_identities(thread_id, user_id, identity_id)
 
 WITH
   new_thread_id AS
-    (INSERT INTO threads(parent_board,title) VALUES (
-      (SELECT id FROM boards WHERE slug = 'gore'), 
-       'Favorite murder scene in videogames?')
+    (INSERT INTO threads(string_id,parent_board,title)
+      VALUES (
+        'a5c903df-35e8-43b2-a41a-208c43154671',
+        (SELECT id FROM boards WHERE slug = 'gore'), 
+        'Favorite murder scene in videogames?')
      RETURNING id),
   posts_insert AS 
     (INSERT INTO posts(string_id, parent_thread, author, content, type, whisper_tags, anonymity_type, created)
       VALUES
-        (uuid_generate_v4(), 
+        ('89fc3682-cb74-43f9-9a63-bd97d0f59bb9', 
         (SELECT id FROM new_thread_id),
         (SELECT id FROM Users WHERE username = 'oncest5evah'),
         '[{"insert":"Everything in The Evil Within tbh"}]', 
         'text', 
-        ARRAY['fight me on this'], 
+        ARRAY['joseph oda is love', 'joseph oda is life'], 
         'strangers',
-        now()),
-        (uuid_generate_v4(), 
+        to_timestamp('2020-04-30 05:42:00', 'YYYY-MM-DD HH:MI:SS')),
+        ('1f1ad4fa-f02a-48c0-a78a-51221a7db170', 
         (SELECT id FROM new_thread_id),
         (SELECT id FROM Users WHERE username = 'jersey_devil_69'),
         '[{"insert":"(chants) Leon Kennedy! Leon Kennedy! Leon Kennedy!)"}]', 
         'text', 
-        ARRAY['Im too ashamed to admit this ok', 'sorry mom', 'YOU WILL NEVER KNOW WHO I AM'], 
-        'everyone',
-        now() + INTERVAL'5 minute'))
+        ARRAY['nothing beats a himbo getting gangbanged by a herd of hungry hungry zombies'], 
+        'strangers',
+        to_timestamp('2020-04-30 05:42:00', 'YYYY-MM-DD HH:MI:SS') + INTERVAL'5 minute'))
 INSERT INTO user_thread_identities(thread_id, user_id, identity_id)
     VALUES
      ((SELECT id FROM new_thread_id),
