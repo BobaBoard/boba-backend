d8fd1c6df6179d0651f2447072f7f173c7535d75
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-05-28 01:40:42 -0700

    add CORS

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,16 @@
 import debug from "debug";
 import express from "express";
+import cors from "cors";
 import { applyRoutes } from "./all-routes";
 
 const log = debug("bobaserver:main");
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 4200;
 
 applyRoutes(app);
+app.set("json spaces", 2);
 
 if (require.main === module) {
   app.listen(port, () =>
