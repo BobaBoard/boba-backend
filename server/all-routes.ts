2449a8e78195b560178d9b42d1d06c19ea9e1f87
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-05-29 15:43:37 -0700

    create endpoint for new contribution

--- server/all-routes.ts
+++ server/all-routes.ts
@@ -2,12 +2,14 @@ import debug from "debug";
 import { Router } from "express";
 import BoardRoutes from "./boards/routes";
 import ThreadRoutes from "./threads/routes";
+import PostRoutes from "./posts/routes";
 
 const log = debug("bobaserver:all-routes");
 
 const ROUTES: { [key: string]: Router } = {
   boards: BoardRoutes,
   threads: ThreadRoutes,
+  posts: PostRoutes,
 };
 
 export const applyRoutes = (router: Router) => {
