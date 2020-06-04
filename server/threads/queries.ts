8b5435356ab1fb07f670e6c1dc6732758ef50f06
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-06-03 19:27:28 -0700

    add create thread rules

--- server/threads/queries.ts
+++ server/threads/queries.ts
@@ -1,5 +1,6 @@
 import debug from "debug";
 import pool from "../pool";
+import { v4 as uuidv4 } from "uuid";
 
 const log = debug("bobaserver:threads:queries-log");
 const error = debug("bobaserver:threads:queries-error");
@@ -74,5 +75,73 @@ export const getThreadIdentitiesByStringId = async (
     error(`Error while fetching boards.`);
     error(e);
     return null;
+  }
+};
+
+export const createThread = async ({
+  userId,
+  content,
+  anonymityType,
+  boardSlug,
+}: {
+  userId: string;
+  content: string;
+  anonymityType: string;
+  boardSlug: string;
+}) => {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const userQuery = "SELECT id FROM users WHERE firebase_id = $1 LIMIT 1";
+    const userRes = await client.query(userQuery, [userId]);
+    const threadStringId = uuidv4();
+    const createThread = `
+      INSERT INTO threads(string_id, parent_board)
+      VALUES (
+        $1,
+        (SELECT id FROM boards WHERE slug = $2))
+      RETURNING id`;
+    const threadResult = await client.query(createThread, [
+      threadStringId,
+      boardSlug,
+    ]);
+
+    const createPost = `
+    INSERT INTO posts(string_id, parent_post, parent_thread, author, content, type, 
+                      whisper_tags, anonymity_type)
+    VALUES
+      ($1,
+       NULL,
+       $2,
+       $3,
+       $4, 
+       'text', 
+       NULL, 
+       $5)`;
+    const creatPostResult = await client.query(createPost, [
+      uuidv4(),
+      threadResult.rows[0].id,
+      userRes.rows[0].id,
+      content,
+      anonymityType,
+    ]);
+
+    const randomIdentity =
+      "SELECT id FROM secret_identities ORDER BY RANDOM() LIMIT 1";
+    const identityRes = await client.query(randomIdentity);
+
+    const createIdentityResult = await client.query(
+      `INSERT INTO user_thread_identities(thread_id, user_id, identity_id)
+       VALUES($1, $2, $3)`,
+      [threadResult.rows[0].id, userRes.rows[0].id, identityRes.rows[0].id]
+    );
+    await client.query("COMMIT");
+    return threadStringId;
+  } catch (e) {
+    await client.query("ROLLBACK");
+    error(e);
+    return null;
+  } finally {
+    client.release();
   }
 };
--- server/threads/routes.ts
+++ server/threads/routes.ts
@@ -1,6 +1,11 @@
 import debug from "debug";
 import express from "express";
-import { getThreadByStringId, getThreadIdentitiesByStringId } from "./queries";
+import {
+  getThreadByStringId,
+  getThreadIdentitiesByStringId,
+  createThread,
+} from "./queries";
+import { isLoggedIn } from "../auth-handler";
 
 const info = debug("bobaserver:threads:routes:info");
 const log = debug("bobaserver:threads:routes");
@@ -31,6 +36,27 @@ router.get("/:id", async (req, res) => {
 router.get("/activity/latest", async (req, res) => {
   // TODO: implement. Gets latest active threads.
   res.status(501);
+});
+
+router.post("/:boardSlug/create", isLoggedIn, async (req, res) => {
+  const { boardSlug } = req.params;
+  log(`Creating thread in board with slug ${boardSlug}`);
+  const { content, forceAnonymous } = req.body;
+
+  const threadStringId = await createThread({
+    // @ts-ignore
+    userId: req.currentUser.uid,
+    content,
+    anonymityType: "everyone",
+    boardSlug: boardSlug,
+  });
+  info(`Created new thread %O`, threadStringId);
+
+  if (!threadStringId) {
+    res.sendStatus(500);
+    return;
+  }
+  res.status(200).json(await getThreadByStringId(threadStringId));
 });
 
 export default router;
