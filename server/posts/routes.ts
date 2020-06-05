58e3cd3f6ca598c4677586d1eea943e9a6e06bf1
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-06-04 23:42:55 -0700

    create comment endpoint for backend

--- server/posts/routes.ts
+++ server/posts/routes.ts
@@ -1,6 +1,6 @@
 import debug from "debug";
 import express from "express";
-import { postNewContribution } from "./queries";
+import { postNewContribution, postNewComment } from "./queries";
 import { isLoggedIn } from "../auth-handler";
 
 const info = debug("bobaserver:posts:routes:info");
@@ -26,6 +26,40 @@ router.post("/:postId/contribute", isLoggedIn, async (req, res) => {
   log(`Anonymous: `, forceAnonymous);
 
   const post = await postNewContribution({
+    // @ts-ignore
+    userId: 1,
+    replyTo: postId,
+    content,
+    anonymityType: forceAnonymous ? "everyone" : "strangers",
+  });
+  log(`Contribution posted: `, post);
+
+  if (!post) {
+    res.sendStatus(500);
+    return;
+  }
+  res.status(200).json(post);
+});
+
+router.post("/:postId/comment", isLoggedIn, async (req, res) => {
+  const { postId } = req.params;
+  log(`body: `, req.body);
+  // @ts-ignore
+  log(`user`, req.currentUser);
+  const { content, forceAnonymous } = req.body;
+
+  // @ts-ignore
+  if (!req.currentUser) {
+    res.sendStatus(403);
+    return;
+  }
+
+  log(`Making comment to post with id ${postId}`);
+  log(`Content: `, content);
+  log(`Anonymous: `, forceAnonymous);
+
+  const post = await postNewComment({
+    // @ts-ignore
     userId: 1,
     replyTo: postId,
     content,
