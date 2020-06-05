b0792ab0d5fd2eb15ec535245b03b119ac714a89
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-06-05 13:33:51 -0700

    allow deployment of server to production

--- server/pool.ts
+++ server/pool.ts
@@ -3,10 +3,21 @@ import { Pool } from "pg";
 
 const log = debug("bobaserver:pool");
 
-const DATABASE_URL = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@127.0.0.1:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-
-const databaseConfig = { connectionString: DATABASE_URL };
-log(`attempting connection to the db on port ${process.env.POSTGRES_PORT}`);
+let databaseConfig;
+log(`Attempting db connection...`);
+if (process.env.NODE_ENV == "production") {
+  log(`Connecting to remove database host: ${process.env.POSTGRES_HOST}`);
+  databaseConfig = {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  };
+} else {
+  log(`Attempting connection to local db on port ${process.env.POSTGRES_PORT}`);
+  const DATABASE_URL = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@127.0.0.1:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+  databaseConfig = { connectionString: DATABASE_URL };
+}
 const pool = new Pool(databaseConfig);
 
 pool.on("connect", () => {
