4f1863789648b7decb28539b144348920590e874
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-06-08 04:38:18 -0700

    add has updates to boards check endpoint

--- server/boards/queries.ts
+++ server/boards/queries.ts
@@ -4,20 +4,41 @@ import pool from "../pool";
 const log = debug("bobaserver:board:queries-log");
 const error = debug("bobaserver:board:queries-error");
 
-export const getBoards = async (): Promise<any> => {
+export const getBoards = async ({
+  firebaseId,
+}: {
+  firebaseId: string;
+}): Promise<any> => {
   const query = `
     SELECT 
         boards.slug,
         boards.tagline,
         boards.avatar_reference_id,
         boards.settings,
-        COUNT(threads.id) as threads_count
+        COUNT(threads.id) as threads_count,
+        MAX(posts.last_activity) as last_post,
+        MAX(comments.last_activity) as last_comment,
+        MAX(user_board_last_visits.last_visit_time) last_visit,
+        CASE 
+          WHEN MAX(posts.last_activity) IS NOT NULL OR MAX(comments.last_activity) IS NOT NULL
+          THEN COALESCE(MAX(user_board_last_visits.last_visit_time), '-infinity') < GREATEST(MAX(posts.last_activity), MAX(comments.last_activity))
+          ELSE false
+        END as has_updates
     FROM boards
-    LEFT JOIN threads ON boards.id = threads.parent_board
+    LEFT JOIN threads 
+      ON boards.id = threads.parent_board
+    LEFT JOIN user_board_last_visits
+      ON user_board_last_visits.board_id = boards.id AND user_board_last_visits.user_id = (SELECT id FROM users WHERE users.firebase_id = $1)
+    LEFT JOIN LATERAL (
+      SELECT MAX(created) as last_activity FROM posts WHERE posts.parent_thread = threads.id) as posts
+      ON 1=1
+    LEFT JOIN LATERAL (
+      SELECT MAX(created) as last_activity FROM comments WHERE comments.parent_thread = threads.id) as comments
+      ON 1=1
     GROUP BY boards.id`;
 
   try {
-    const { rows } = await pool.query(query);
+    const { rows } = await pool.query(query, [firebaseId]);
     return rows;
   } catch (e) {
     error(`Error while fetching boards.`);
