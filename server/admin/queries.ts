d4cafb84b933f21f6cfb6240ddb3744440b3f101
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-06-09 19:48:02 -0700

    add method to generate identities from spreadsheets

--- server/admin/queries.ts
+++ server/admin/queries.ts
@@ -4,7 +4,44 @@ import pool from "../pool";
 const log = debug("bobaserver:admin:queries-log");
 const error = debug("bobaserver:admin:queries-error");
 
-export const createBoardsIfNotExists = async (
+export const createIdentitiesIfNotExist = async (
+  identities: {
+    avatar?: string;
+    name?: string;
+  }[]
+) => {
+  const query = `
+    INSERT INTO secret_identities(display_name, avatar_reference_id) VALUES (
+      $1,
+      $2)
+    ON CONFLICT(display_name) DO NOTHING`;
+  try {
+    const promises = identities.map(({ name, avatar }) => {
+      log(`Attempting to insert identity: ${name}`);
+      log(`data: `, {
+        name,
+        avatar,
+      });
+      if (!(name && avatar)) {
+        log(`...data missing.`);
+        return null;
+      } else return pool.query(query, [name, avatar]);
+    });
+    const result = await Promise.all(promises);
+    const recordsAdded = result.reduce(
+      (value, row) => value + (row?.rowCount || 0),
+      0
+    );
+    log(`Added ${recordsAdded} records.`);
+    return recordsAdded;
+  } catch (e) {
+    error(`Error while adding new identities.`);
+    error(e);
+    return false;
+  }
+};
+
+export const createBoardsIfNotExist = async (
   boards: {
     slug?: string;
     tagline?: string;
--- server/admin/routes.ts
+++ server/admin/routes.ts
@@ -1,7 +1,7 @@
 import debug from "debug";
 import express from "express";
 import axios from "axios";
-import { createBoardsIfNotExists } from "./queries";
+import { createBoardsIfNotExist, createIdentitiesIfNotExist } from "./queries";
 import { isLoggedIn } from "../auth-handler";
 // import { transformImageUrls, mergeActivityIdentities } from "../response-utils";
 
@@ -11,6 +11,7 @@ const router = express.Router();
 
 const ADMIN_ID = "c6HimTlg2RhVH3fC1psXZORdLcx2";
 const BOARDS_SHEET_ID = "1ikA8dgtKAHuf-3FVrCfwWZ-0hL-1fMd7u1mGvLz4_no";
+const IDENTITIES_SHEET_ID = "1I3xXEQTDrp_XVpYWCTtTj106F2ad-PW__474NeqAHvs";
 const API_KEY = "AIzaSyA2KQh1wqrLwsrWvKQvFWeWoWMR8KOyTD4";
 const getSheetUrl = (url: string) =>
   `https://sheets.googleapis.com/v4/spreadsheets/${url}/?key=${API_KEY}&includeGridData=true`;
@@ -22,25 +23,50 @@ router.get("/generate/boards", isLoggedIn, async (req, res) => {
     return res.sendStatus(401);
   }
 
-  const data = await getSpreadsheetData(getSheetUrl(BOARDS_SHEET_ID));
-  const recordsAdded = createBoardsIfNotExists(data);
+  const data = await getSpreadsheetData(
+    getSheetUrl(BOARDS_SHEET_ID),
+    (rowData, i) => ({
+      slug: rowData[i].values?.[0]?.formattedValue,
+      tagline: rowData[i].values?.[1]?.formattedValue,
+      avatar: rowData[i]?.values?.[2]?.formattedValue,
+      accent: rowData[i]?.values?.[3]?.formattedValue,
+    })
+  );
+  const recordsAdded = await createBoardsIfNotExist(data);
 
   res.status(200).json({ added: recordsAdded });
 });
 
-const getSpreadsheetData = (url: string) => {
+router.get("/generate/identities", isLoggedIn, async (req, res) => {
+  // @ts-ignore
+  if (req.currentUser?.uid !== ADMIN_ID) {
+    // TODO: fix wrong status
+    return res.sendStatus(401);
+  }
+
+  const data = await getSpreadsheetData(
+    getSheetUrl(IDENTITIES_SHEET_ID),
+    (rowData, i) => ({
+      name: rowData[i].values?.[0]?.formattedValue,
+      avatar: rowData[i].values?.[1]?.formattedValue,
+    })
+  );
+  const recordsAdded = await createIdentitiesIfNotExist(data);
+
+  res.status(200).json({ added: recordsAdded });
+});
+
+const getSpreadsheetData = (
+  url: string,
+  transform: (rowData: any, index: number) => any
+) => {
   return axios.get(url).then((response) => {
     const rowData = response.data.sheets[0].data[0].rowData;
     let hasData = true;
     let i = 1;
     const rows = [];
     while (hasData && i < rowData.length) {
-      rows.push({
-        slug: rowData[i].values?.[0]?.formattedValue,
-        tagline: rowData[i].values?.[1]?.formattedValue,
-        avatar: rowData[i]?.values?.[2]?.formattedValue,
-        accent: rowData[i]?.values?.[3]?.formattedValue,
-      });
+      rows.push(transform(rowData, i));
       i++;
       hasData = !!rowData[i].values[1]?.formattedValue;
     }
