2a2dca2a5c3344cb98d22c7f78ffc38d906e461d
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-06-21 16:02:42 -0700

    add dismiss notifications query

--- server/users/queries.ts
+++ server/users/queries.ts
@@ -22,3 +22,27 @@ export const getUserFromFirebaseId = async ({
     return null;
   }
 };
+
+export const dismissAllNotifications = async ({
+  firebaseId,
+}: {
+  firebaseId: string;
+}): Promise<any> => {
+  const dismissNotifications = `
+    INSERT INTO dismiss_notifications_requests(user_id, dismiss_request_time) VALUES (
+        (SELECT id FROM users WHERE users.firebase_id = $/firebase_id/),
+         DEFAULT)
+    ON CONFLICT(user_id) DO UPDATE 
+        SET dismiss_request_time = DEFAULT
+        WHERE dismiss_notifications_requests.user_id = (SELECT id FROM users WHERE users.firebase_id = $/firebase_id/)`;
+
+  try {
+    await pool.none(dismissNotifications, { firebase_id: firebaseId });
+    info(`Dismissed all notifications for user with firebaseId: `, firebaseId);
+    return true;
+  } catch (e) {
+    error(`Error while dismissing notifications.`);
+    error(e);
+    return false;
+  }
+};
--- server/users/routes.ts
+++ server/users/routes.ts
@@ -1,6 +1,6 @@
 import debug from "debug";
 import express from "express";
-import { getUserFromFirebaseId } from "./queries";
+import { getUserFromFirebaseId, dismissAllNotifications } from "./queries";
 import { isLoggedIn } from "../auth-handler";
 import { transformImageUrls } from "../response-utils";
 
@@ -28,6 +28,27 @@ router.get("/me", isLoggedIn, async (req, res) => {
     username: userData.username,
     avatarUrl: userData.avatarUrl,
   });
+});
+
+router.post("/notifications/dismiss", isLoggedIn, async (req, res) => {
+  // @ts-ignore
+  let currentUserId: string = req.currentUser?.uid;
+  if (!currentUserId) {
+    res.sendStatus(401);
+  }
+  log(`Dismissing notifications for firebase id: ${currentUserId}`);
+  const dismissSuccessful = await dismissAllNotifications({
+    firebaseId: currentUserId,
+  });
+
+  if (!dismissSuccessful) {
+    error(`Dismiss failed`);
+    return res.sendStatus(500);
+  }
+
+  info(`Dismiss successful`);
+
+  res.sendStatus(204);
 });
 
 export default router;
