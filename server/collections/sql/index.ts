1c3870212221709f22350d95acb9113d92bd361b
Essential Randomness <essential.randomn3ss@gmail.com>
2020-08-20 19:36:24 -0700

    set up collections routes

---
+++ server/collections/sql/index.ts
@@ -0,0 +1,8 @@
+import pg, { QueryFile } from "pg-promise";
+import path from "path";
+
+export default {
+  //   getThreadDetails: new QueryFile(
+  //     path.join(__dirname, "get-thread-details.sql")
+  //   ),
+};
