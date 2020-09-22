f37834f5cbbaee2762bcf3591ef7b4b0cd8130aa
Essential Randomness <essential.randomn3ss@gmail.com>
2020-09-22 16:39:46 -0700

    add descriptions metadata extraction

---
+++ server/boards/utils.ts
@@ -0,0 +1,101 @@
+import { BoardDescription, BoardMetadata } from "../../Types";
+
+/**
+ * Returns a delta between the old metadata of a Board and the new one.
+ */
+export const getMetadataDelta = ({
+  oldMetadata,
+  newMetadata,
+}: {
+  oldMetadata: BoardMetadata;
+  newMetadata: BoardMetadata;
+}): {
+  texts: {
+    deleted: { id: number }[];
+    newAndUpdated: {}[];
+  };
+  categoryFilters: {
+    deleted: { id: number }[];
+    newAndUpdated: {
+      categories: {
+        deleted: string[];
+        new: string[];
+      };
+    }[];
+  };
+} => {
+  const oldTexts = oldMetadata.descriptions.filter(
+    (desc) => desc.type == "text"
+  );
+  const oldCategoryFilters = oldMetadata.descriptions.filter(
+    (desc) => desc.type == "category_filter"
+  );
+  const newTexts = newMetadata.descriptions.filter(
+    (desc) => desc.type == "text"
+  );
+  const newCategoryFilters = newMetadata.descriptions.filter(
+    (desc) => desc.type == "category_filter"
+  );
+
+  // Deleted texts will be in oldTexts but not newTexts
+  const deletedTexts = oldTexts.filter(
+    (oldText) => !newTexts.some((newText) => newText.id == oldText.id)
+  );
+  // There's no special handling needed for new and updated texts.
+  const newAndUpdatedTexts = newTexts;
+
+  // Deleted filters will be in oldCategoryFilters but not newCategoryFilters
+  const deletedFilters = oldCategoryFilters.filter(
+    (oldFilter) =>
+      !newCategoryFilters.some((newFilter) => newFilter.id == oldFilter.id)
+  );
+
+  const newAndUpdatedFilters = newCategoryFilters.map((newFilter) => {
+    const oldFilter = oldCategoryFilters.find(
+      (oldFilter) => oldFilter.id == newFilter.id
+    );
+
+    // Deleted categories will be in oldFilter's but not newFilter's.
+    // If there's no oldFilter, by definition nothing will have been deleted.
+    const deletedCategories = oldFilter
+      ? oldFilter.categories.filter(
+          (oldCategory) =>
+            !newFilter.categories.some(
+              (newCategory) =>
+                oldCategory.toLowerCase() == newCategory.toLowerCase()
+            )
+        )
+      : [];
+
+    // New categories will be in newFilter's but not oldFilter's.
+    // If there's no oldFilter, by definition everything will be new
+    const newCategories = oldFilter
+      ? newFilter.categories.filter(
+          (newCategory) =>
+            !oldFilter.categories.some(
+              (oldCategory) =>
+                oldCategory.toLowerCase() == newCategory.toLowerCase()
+            )
+        )
+      : newFilter.categories;
+
+    return {
+      ...newFilter,
+      categories: {
+        deleted: deletedCategories,
+        new: newCategories,
+      },
+    };
+  });
+
+  return {
+    texts: {
+      deleted: deletedTexts.map((text) => ({ id: text.id })),
+      newAndUpdated: newAndUpdatedTexts,
+    },
+    categoryFilters: {
+      deleted: deletedFilters.map((filter) => ({ id: filter.id })),
+      newAndUpdated: newAndUpdatedFilters,
+    },
+  };
+};
