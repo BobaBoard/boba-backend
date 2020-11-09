ab42bcb504188b6c52fedbe9f678e5acf133a294
Quicksilver Mercurial <quicksilver.mercurial@gmail.com>
2020-11-08 20:39:22 -0800

    Add querystringParamToArray function

--- server/tags/routes.ts
+++ server/tags/routes.ts
@@ -1,9 +1,24 @@
+import debug from "debug";
 import express from "express";
 import {
   getPostsWithTags
 } from "./queries"; 
 
 const router = express.Router();
+const log = debug("bobaserver:tags:routes-log");
+
+function querystringParamToArray(param: any) {
+  switch(typeof param) {
+    case 'string':
+      return [param];
+    case 'object':
+      if (Array.isArray(param)) {
+        return param
+      }
+    default:
+      return [];
+  }
+}
 
 router.get("/search", async (req, res) => {
   const includeTags = req.query.tags as string;
