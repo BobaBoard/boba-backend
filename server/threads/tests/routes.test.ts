744822dceaec6e50d3ea0f2b1c4e7fd9c35c54b8
Essential Randomness <essential.randomn3ss@gmail.com>
2020-11-10 21:17:10 -0800

    get typescript checks to work again

--- server/threads/tests/routes.test.ts
+++ server/threads/tests/routes.test.ts
@@ -11,7 +11,7 @@ describe("Tests threads REST API", () => {
   beforeEach(function (done) {
     app = express();
     app.use(router);
-    listener = app.listen(4000, (err) => {
+    listener = app.listen(4000, () => {
       done();
     });
   });
