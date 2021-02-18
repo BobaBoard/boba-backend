23358a92f7825f99de30765807b6966b0b57e50d
Essential Randomness <essential.randomn3ss@gmail.com>
2021-02-17 16:10:58 -0800

    fix subscription fetching

--- server/subscriptions/sql/subscription-activity-by-string-id.sql
+++ server/subscriptions/sql/subscription-activity-by-string-id.sql
@@ -44,7 +44,7 @@ board_subscription_threads AS (
     INNER JOIN LATERAL (SELECT MAX(posts.created) AS update_time FROM posts WHERE posts.parent_thread = threads.id) AS last_post ON 1=1
     INNER JOIN LATERAL (SELECT MAX(comments.created) AS update_time FROM comments WHERE comments.parent_thread = threads.id) AS last_comment ON 1=1
     INNER JOIN post_categories ON top_posts.id = post_categories.post_id AND bcm.category_id = post_categories.category_id
-    INNER JOIN thread_identities ON threads.id = thread_identities.thread_id
+    LEFT JOIN thread_identities ON threads.id = thread_identities.thread_id
   WHERE subscriptions.string_id = ${subscription_string_id}
     AND GREATEST(last_post.update_time, last_comment.update_time) <= COALESCE(${last_activity_cursor}, NOW())
   ORDER BY last_updated DESC
@@ -67,7 +67,7 @@ thread_subscription_threads AS (
   INNER JOIN threads ON threads.id = tcs.thread_id
   INNER JOIN posts ON posts.parent_thread = tcs.thread_id
   INNER JOIN post_categories ON posts.id = post_categories.post_id AND tcs.category_id = post_categories.category_id 
-  INNER JOIN thread_identities ON threads.id = thread_identities.thread_id
+  LEFT JOIN thread_identities ON threads.id = thread_identities.thread_id
   WHERE subscriptions.string_id = ${subscription_string_id}
     AND posts.created <= COALESCE(${last_activity_cursor}, NOW())
   ORDER BY last_updated DESC 
