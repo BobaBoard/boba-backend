5077710c289cb1af2b04ca1ebef812f52cd986b1
Essential Randomness <essential.randomn3ss@gmail.com>
2021-10-14 16:41:12 -0700

    make all tests pass

--- server/boards/tests/restricted.test.ts
+++ server/boards/tests/restricted.test.ts
@@ -1,8 +1,4 @@
-import express, { Express } from "express";
 import { getBoardBySlug, getBoards } from "../queries";
-
-import { Server } from "http";
-import router from "../routes";
 
 const extractRestrictions = (board: any) => {
   return {
@@ -12,55 +8,31 @@ const extractRestrictions = (board: any) => {
 };
 
 describe("Tests restricted board queries", () => {
-  let app: Express;
-  let listener: Server;
-  beforeEach((done) => {
-    app = express();
-    app.use(router);
-    listener = app.listen(4000, () => {
-      done();
+  test("board fetch contains lock access restriction for logged out users", async () => {
+    const board = await getBoardBySlug({
+      slug: "restricted",
+      // Oncest
+      firebaseId: "fb3",
     });
-    process.env.FORCED_USER = undefined;
+
+    expect(extractRestrictions(board)).toEqual({
+      logged_in_base_restrictions: [],
+      logged_out_restrictions: ["lock_access"],
+    });
   });
-  afterEach((done) => {
-    listener.close(done);
-    process.env.FORCED_USER = undefined;
-  });
-  describe("tests logged-in-only board fetch", () => {
-    describe("DB queries", () => {
-      it(
-        "board fetch contains lock access restriction for logged out users",
-        async () => {
-          const board = await getBoardBySlug({
-            slug: "restricted",
-            // Oncest
-            firebaseId: "fb3",
-          });
 
-          expect(extractRestrictions(board)).toEqual({
-            logged_in_base_restrictions: [],
-            logged_out_restrictions: ["lock_access"],
-          });
-        }
-      );
+  test("board fetch contains lock access restriction for logged out users", async () => {
+    const boards = await getBoards({
+      firebaseId: "fb3",
+    });
 
-      it(
-        "board fetch contains lock access restriction for logged out users",
-        async () => {
-          const boards = await getBoards({
-            firebaseId: "fb3",
-          });
-
-          expect(
-            extractRestrictions(
-              boards.find((board: any) => board.slug == "restricted")
-            )
-          ).toEqual({
-            logged_in_base_restrictions: [],
-            logged_out_restrictions: ["lock_access"],
-          });
-        }
-      );
+    expect(
+      extractRestrictions(
+        boards.find((board: any) => board.slug == "restricted")
+      )
+    ).toEqual({
+      logged_in_base_restrictions: [],
+      logged_out_restrictions: ["lock_access"],
     });
   });
 });
