cc8217b89f61ac79c5aedd8185e06877304bac17
Essential Randomness <essential.randomn3ss@gmail.com>
2021-11-03 13:08:48 -0700

    better mocking of db-pool

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -20,32 +20,21 @@ export const runWithinTransaction = async (
 };
 
 export const wrapWithTransaction = async (test: () => void) => {
-  jest.mock("server/db-pool", () => ({
-    ...jest.requireActual("server/db-pool").default,
-    tx: (name: string, transaction: any) =>
-      // Because nested transactions are committed the moment they're done, even
-      // when a parent transaction exists, we need to make sure that transactions
-      // are not actually executed as such during tests. We then turn them into
-      // conditional transactions where the condition is always false. This
-      // is functionally equivalent (aside from the transaction part).
-      jest.requireActual("server/db-pool").default.txIf(
-        {
-          tag: name,
-          cnd: false,
-        },
-        transaction
-      ),
-  }));
+  if (!jest.isMockFunction(pool.tx)) {
+    throw Error(
+      "wrapWithTransaction requires 'server/db-pool' to be explicitly mocked."
+    );
+  }
   try {
     log("starting transaction");
     await pool.none("BEGIN TRANSACTION;");
     await test();
   } finally {
     log("running cleanup");
-    await pool.none("ROLLBACK;");
-    jest.mock("server/db-pool", () => ({
-      ...jest.requireActual("server/db-pool").default,
-    }));
+    //   await pool.none("ROLLBACK;");
+    //   jest.mock("server/db-pool", () => ({
+    //     ...jest.requireActual("server/db-pool").default,
+    //   }));
   }
 };
 
