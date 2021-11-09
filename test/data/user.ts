c0542173eb49528917888a42364bfa84d87efc9f
Essential Randomness <essential.randomn3ss@gmail.com>
2021-11-08 20:46:19 -0800

    add tests for user data

---
+++ test/data/user.ts
@@ -0,0 +1,34 @@
+import { UserData } from "../../types/rest/user";
+
+export const BOBATAN_USER_DATA: UserData = {
+  avatar_url: "/bobatan.png",
+  pinned_boards: {
+    anime: {
+      accent_color: "#24d282",
+      avatar_url: "/anime.png",
+      delisted: false,
+      id: "4b30fb7c-2aca-4333-aa56-ae8623a92b65",
+      index: "2",
+      logged_in_only: false,
+      muted: false,
+      pinned: true,
+      realm_id: "v0-fake-id",
+      slug: "anime",
+      tagline: "I wish I had a funny one for this.",
+    },
+    gore: {
+      accent_color: "#f96680",
+      avatar_url: "/gore.png",
+      delisted: false,
+      id: "c6d3d10e-8e49-4d73-b28a-9d652b41beec",
+      index: "1",
+      logged_in_only: false,
+      muted: false,
+      pinned: true,
+      realm_id: "v0-fake-id",
+      slug: "gore",
+      tagline: "Blood! Blood! Blood!",
+    },
+  },
+  username: "bobatan",
+};
