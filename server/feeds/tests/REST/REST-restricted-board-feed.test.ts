e1854a9a8ef49605b706eb1b8caf123eb8b456a4
Essential Randomness <essential.randomn3ss@gmail.com>
2021-11-12 18:08:03 -0800

    add more tests to board feed and thread

--- server/feeds/tests/REST/REST-restricted-board-feed.test.ts
+++ server/feeds/tests/REST/REST-restricted-board-feed.test.ts
@@ -3,6 +3,7 @@ import { setLoggedInUser, startTestServer } from "utils/test-utils";
 import { BOBATAN_USER_ID } from "test/data/auth";
 import { RESTRICTED_BOARD_ID } from "test/data/boards";
 import { RESTRICTED_THREAD_SUMMARY } from "test/data/threads";
+import clone from "clone";
 import request from "supertest";
 import router from "../../routes";
 
@@ -14,7 +15,7 @@ describe("Test feed of restricted boards REST API", () => {
   test("doesn't fetch board activity when logged out (REST)", async () => {
     const res = await request(server.app).get(`/boards/${RESTRICTED_BOARD_ID}`);
 
-    expect(res.status).toBe(403);
+    expect(res.status).toBe(401);
     expect(res.body).toEqual({
       message: "User does not have required permissions to access board.",
     });
@@ -25,8 +26,14 @@ describe("Test feed of restricted boards REST API", () => {
     const res = await request(server.app).get(`/boards/${RESTRICTED_BOARD_ID}`);
 
     expect(res.status).toBe(200);
+
+    const ACTIVITY_THREAD_SUMMARY = clone(RESTRICTED_THREAD_SUMMARY);
+    // This data is not fetched for the starter thread on the feed.
+    ACTIVITY_THREAD_SUMMARY.starter.new_comments_amount = 0;
+    ACTIVITY_THREAD_SUMMARY.starter.total_comments_amount = 0;
+
     expect(res.body).toEqual({
-      activity: [RESTRICTED_THREAD_SUMMARY],
+      activity: [ACTIVITY_THREAD_SUMMARY],
       cursor: {
         next: null,
       },
