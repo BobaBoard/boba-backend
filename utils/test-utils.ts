d091bdacea299798e009e25b2c25b747f7f84aaf
Essential Randomness <essential.randomn3ss@gmail.com>
2021-11-17 16:59:13 -0800

    fix categories and update tests

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -14,8 +14,11 @@ export const runWithinTransaction = async (
   test: (transaction: ITask<any>) => void
 ) => {
   await pool.tx("test-transaction", async (t) => {
-    await test(t);
-    await t.none("ROLLBACK;");
+    try {
+      await test(t);
+    } finally {
+      await t.none("ROLLBACK;");
+    }
   });
 };
 
