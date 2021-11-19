563325e822cbbfda97c88aa5410b1df83301d2d8
Sel <dimensionstraveler@gmail.com>
2021-11-19 16:19:54 +0800

    Added tests for several thread endpoints

--- server/threads/tests/routes/get-thread.test.ts
+++ server/threads/tests/routes/get-thread.test.ts
@@ -3,11 +3,15 @@ import {
   FAVORITE_CHARACTER_THREAD_ID,
   RESTRICTED_THREAD,
   RESTRICTED_THREAD_ID,
+  NULL_ID,
+  NULL_THREAD_NOT_FOUND,
 } from "test/data/threads";
+
 import { setLoggedInUser, startTestServer } from "utils/test-utils";
 
 import { BOBATAN_USER_ID } from "test/data/auth";
 import { Thread } from "types/rest/threads";
+import { GenericResponse } from "types/rest/responses";
 import request from "supertest";
 import router from "../../routes";
 
@@ -37,5 +41,19 @@ describe("Tests threads REST API", () => {
 
     expect(res.status).toBe(200);
     expect(res.body).toEqual<Thread>(RESTRICTED_THREAD);
+  });
+
+  test("TODO: should lock when user does not have access to board", async () => {
+    //setLoggedInUser(BOBATAN_USER_ID);
+    //expect(res.status).toBe(403);
+    //expect(res.body).toEqual<Thread>(RESTRICTED_THREAD);
+  });
+
+  test("should fail when thread does not exist", async () => {
+    setLoggedInUser(BOBATAN_USER_ID);
+    const res = await request(server.app).get(`/${NULL_ID}`);
+
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual<GenericResponse>(NULL_THREAD_NOT_FOUND);
   });
 });
---
+++ server/threads/tests/routes/visit-thread.test.ts
@@ -0,0 +1,74 @@
+import {
+  FAVORITE_CHARACTER_THREAD_ID,
+  RESTRICTED_THREAD_ID,
+  NULL_ID,
+  NULL_THREAD_NOT_FOUND,
+} from "test/data/threads";
+
+import {
+  ENSURE_LOGGED_IN_NO_TOKEN,
+  ENSURE_LOGGED_IN_INVALID_TOKEN,
+  ENSURE_THREAD_ACCESS_UNAUTHORIZED,
+} from "test/data/responses";
+
+import { setLoggedInUser, startTestServer } from "utils/test-utils";
+
+import { BOBATAN_USER_ID } from "test/data/auth";
+import { Thread } from "types/rest/threads";
+import { GenericResponse } from "types/rest/responses";
+import request from "supertest";
+import router from "../../routes";
+
+jest.mock("handlers/auth");
+
+describe("Tests threads REST API - visits", () => {
+  const server = startTestServer(router);
+
+  test("should mark thread as visited", async () => {
+    setLoggedInUser(BOBATAN_USER_ID);
+    const res = await request(server.app).post(
+      `/${FAVORITE_CHARACTER_THREAD_ID}/visits`
+    );
+
+    expect(res.status).toBe(200);
+  });
+
+  test("should fail when user is unauthenticated", async () => {
+    const res = await request(server.app).post(
+      `/${FAVORITE_CHARACTER_THREAD_ID}/visits`
+    );
+
+    expect(res.status).toBe(401);
+    expect(res.body).toEqual<GenericResponse>(ENSURE_LOGGED_IN_NO_TOKEN);
+  });
+
+  // TODO: don't know how to generate invalid token
+  test("TODO: should fail when user has invalid authentication", async () => {
+    //const res = await request(server.app).post(
+    //  `/${FAVORITE_CHARACTER_THREAD_ID}/visits`
+    //);
+
+    //expect(res.status).toBe(401);
+    //expect(res.body).toEqual<GenericResponse>(ENSURE_LOGGED_IN_NO_TOKEN);
+  });
+
+  // TODO: permissions don't exist yet
+  test("TODO: should fail when user is not authorized to access thread", async () => {
+    //const res = await request(server.app).post(
+    //  `/${FAVORITE_CHARACTER_THREAD_ID}/visits`
+    //);
+
+    //expect(res.status).toBe(403);
+    //expect(res.body).toEqual<GenericResponse>(ENSURE_THREAD_ACCESS_UNAUTHORIZED);
+  });
+
+  test("should fail when thread does not exist", async () => {
+    setLoggedInUser(BOBATAN_USER_ID);
+    const res = await request(server.app).post(
+      `/${NULL_ID}/visits`
+    );
+
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual<GenericResponse>(NULL_THREAD_NOT_FOUND);
+  });
+});
