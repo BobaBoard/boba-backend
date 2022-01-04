3a020837ee901de2ef145a67be78092a034daad5
Essential Randomness <essential.randomn3ss@gmail.com>
2022-01-03 22:08:40 -0800

    wrap hide and mute tests that modify the db in transactions

--- server/threads/tests/routes/hide-thread.test.ts
+++ server/threads/tests/routes/hide-thread.test.ts
@@ -1,36 +1,41 @@
 import {
-  FAVORITE_CHARACTER_THREAD_ID,
-  RESTRICTED_THREAD_ID,
-  NULL_ID,
-  NULL_THREAD_NOT_FOUND,
-} from "test/data/threads";
-
-import {
-  ENSURE_LOGGED_IN_NO_TOKEN,
   ENSURE_LOGGED_IN_INVALID_TOKEN,
+  ENSURE_LOGGED_IN_NO_TOKEN,
   ENSURE_THREAD_ACCESS_UNAUTHORIZED,
 } from "test/data/responses";
-
-import { setLoggedInUser, startTestServer } from "utils/test-utils";
+import {
+  FAVORITE_CHARACTER_THREAD_ID,
+  NULL_ID,
+  NULL_THREAD_NOT_FOUND,
+  RESTRICTED_THREAD_ID,
+} from "test/data/threads";
+import {
+  setLoggedInUser,
+  startTestServer,
+  wrapWithTransaction,
+} from "utils/test-utils";
 
 import { BOBATAN_USER_ID } from "test/data/auth";
-import { Thread } from "types/rest/threads";
 import { GenericResponse } from "types/rest/responses";
+import { Thread } from "types/rest/threads";
 import request from "supertest";
 import router from "../../routes";
 
 jest.mock("handlers/auth");
+jest.mock("server/db-pool");
 
 describe("Tests threads REST API - hide", () => {
   const server = startTestServer(router);
 
   test("should hide thread", async () => {
-    setLoggedInUser(BOBATAN_USER_ID);
-    const res = await request(server.app).post(
-      `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
-    );
+    await wrapWithTransaction(async () => {
+      setLoggedInUser(BOBATAN_USER_ID);
+      const res = await request(server.app).post(
+        `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
+      );
 
-    expect(res.status).toBe(200);
+      expect(res.status).toBe(200);
+    });
   });
 
   test("should fail when user is unauthenticated", async () => {
@@ -47,7 +52,6 @@ describe("Tests threads REST API - hide", () => {
     //const res = await request(server.app).post(
     //  `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
     //);
-
     //expect(res.status).toBe(401);
     //expect(res.body).toEqual<GenericResponse>(ENSURE_LOGGED_IN_NO_TOKEN);
   });
@@ -57,16 +61,13 @@ describe("Tests threads REST API - hide", () => {
     //const res = await request(server.app).post(
     //  `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
     //);
-
     //expect(res.status).toBe(403);
     //expect(res.body).toEqual<GenericResponse>(ENSURE_THREAD_ACCESS_UNAUTHORIZED);
   });
 
   test("should fail when thread does not exist", async () => {
     setLoggedInUser(BOBATAN_USER_ID);
-    const res = await request(server.app).post(
-      `/${NULL_ID}/hide`
-    );
+    const res = await request(server.app).post(`/${NULL_ID}/hide`);
 
     expect(res.status).toBe(404);
     expect(res.body).toEqual<GenericResponse>(NULL_THREAD_NOT_FOUND);
@@ -77,12 +78,14 @@ describe("Tests threads REST API - unhide", () => {
   const server = startTestServer(router);
 
   test("should unhide thread", async () => {
-    setLoggedInUser(BOBATAN_USER_ID);
-    const res = await request(server.app).delete(
-      `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
-    );
+    await wrapWithTransaction(async () => {
+      setLoggedInUser(BOBATAN_USER_ID);
+      const res = await request(server.app).delete(
+        `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
+      );
 
-    expect(res.status).toBe(200);
+      expect(res.status).toBe(200);
+    });
   });
 
   test("should fail when user is unauthenticated", async () => {
@@ -99,7 +102,6 @@ describe("Tests threads REST API - unhide", () => {
     //const res = await request(server.app).delete(
     //  `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
     //);
-
     //expect(res.status).toBe(401);
     //expect(res.body).toEqual<GenericResponse>(ENSURE_LOGGED_IN_NO_TOKEN);
   });
@@ -109,16 +111,13 @@ describe("Tests threads REST API - unhide", () => {
     //const res = await request(server.app).delete(
     //  `/${FAVORITE_CHARACTER_THREAD_ID}/hide`
     //);
-
     //expect(res.status).toBe(403);
     //expect(res.body).toEqual<GenericResponse>(ENSURE_THREAD_ACCESS_UNAUTHORIZED);
   });
 
   test("should fail when thread does not exist", async () => {
     setLoggedInUser(BOBATAN_USER_ID);
-    const res = await request(server.app).delete(
-      `/${NULL_ID}/hide`
-    );
+    const res = await request(server.app).delete(`/${NULL_ID}/hide`);
 
     expect(res.status).toBe(404);
     expect(res.body).toEqual<GenericResponse>(NULL_THREAD_NOT_FOUND);
