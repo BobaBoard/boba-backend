6ab23046bfe6b31c3be4981943fe3629d5f2473f
Essential Randomness <essential.randomn3ss@gmail.com>
2022-01-04 18:35:25 -0800

    clean up subscriptions sql

--- db/init/090_subscriptions.sql
+++ db/init/090_subscriptions.sql
@@ -21,30 +21,13 @@ CREATE TABLE IF NOT EXISTS subscription_webhooks
 );
 CREATE UNIQUE INDEX subscription_webhook on subscription_webhooks(subscription_id, webhook_id);
 
-/*
- * A mapping of a category in a board, used for subscriptions.
- *
- * TODO: there is a major problem here where categories are mapped to names, rather than to
- * special IDs within the board. This means that what you're subscribing to is a particular
- * *name* of a category used within a board, and currently admins cannot really do a renaming
- * that keeps all relationships the same (without renaming for every single board).
- *
- * TODO: this can likely directly be folded in board_category_subscription.
- */
-CREATE TABLE IF NOT EXISTS board_category_mappings
+CREATE TABLE IF NOT EXISTS board_category_subscriptions
 (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    subscription_id BIGINT REFERENCES subscriptions(id) ON DELETE RESTRICT NOT NULL,
     board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT NOT NULL,
     category_id BIGINT REFERENCES categories(id) ON DELETE RESTRICT NOT NULL
 );
-CREATE UNIQUE INDEX board_category_mappings_entry on board_category_mappings(category_id, board_id);
-
-CREATE TABLE IF NOT EXISTS board_category_subscriptions
-(
-    board_category_mapping_id BIGINT REFERENCES board_category_mappings(id) ON DELETE RESTRICT NOT NULL,
-    subscription_id BIGINT REFERENCES subscriptions(id) ON DELETE RESTRICT NOT NULL
-);
-CREATE UNIQUE INDEX board_category_subscription on board_category_subscriptions(board_category_mapping_id, subscription_id);
+CREATE UNIQUE INDEX board_category_subscription_entry on board_category_subscriptions(subscription_id, board_id, category_id);
 
 CREATE TABLE IF NOT EXISTS thread_category_subscriptions
 (
