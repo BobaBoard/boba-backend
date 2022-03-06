57d26fa2d38ffe058e7d7c7eea592cdc825c79d7
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-03-06 09:56:49 -0800

    Enable getting user permisions for realm

--- server/realms/sql/index.ts
+++ server/realms/sql/index.ts
@@ -38,7 +38,20 @@ const dismissNotifications = `
         WHERE dismiss_notifications_requests.user_id = (SELECT id FROM users WHERE users.firebase_id = $/firebase_id/)
           AND dismiss_notifications_requests.realm_id = (SELECT id FROM realms WHERE realms.string_id = $/realm_id/)`;
 
+// I added this before realizing I didn't actually need it for what I was doing
+// I can leave it in if it will be helpful in future, or I can delete it?
+const getRealmByUuid = `
+    SELECT * FROM realms WHERE id = $/realm_id/`;
+
+const getUserPermissionsForRealm = `
+    SELECT permissions 
+    FROM realm_user_roles 
+      JOIN roles ON role_id = id 
+    WHERE user_id = $/user_id/ AND realm_id = $/realm_id/`;
+
 export default {
   getRealmBySlug,
   dismissNotifications,
+  getRealmByUuid,
+  getUserPermissionsForRealm,
 };
