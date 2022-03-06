2cc8ef804d38ebd92faea9dd6876b681a44472b3
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-03-06 09:56:49 -0800

    Enable getting user permisions for realm

--- server/realms/sql/index.ts
+++ server/realms/sql/index.ts
@@ -1,6 +1,19 @@
 const getRealmBySlug = `
     SELECT * FROM realms WHERE slug = $/realm_slug/`;
 
+// I added this before realizing I didn't actually need it for what I was doing
+// I can leave it in if it will be helpful in future, or I can delete it?
+const getRealmByUuid = `
+    SELECT * FROM realms WHERE id = $/realm_id/`;
+
+const getUserPermissionsForRealm = `
+    SELECT permissions 
+    FROM realm_user_roles 
+      JOIN roles ON role_id = id 
+    WHERE user_id = $/user_id/ AND realm_id = $/realm_id/`;
+
 export default {
   getRealmBySlug,
+  getRealmByUuid,
+  getUserPermissionsForRealm,
 };
