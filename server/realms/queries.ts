a9d64f67fd7ee88ab14ee1ffcd807ceaaaa57da4
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-03-07 06:28:42 -0800

    move invite endpoints to realms and gate creation

--- server/realms/queries.ts
+++ server/realms/queries.ts
@@ -9,9 +9,9 @@ import { extractRealmPermissions } from "utils/permissions-utils";
 import pool from "server/db-pool";
 import sql from "./sql";
 
-const log = debug("bobaserver:users:queries-log");
-const error = debug("bobaserver:users:queries-error");
-const info = debug("bobaserver:users:queries-info");
+const info = debug("bobaserver:realms:queries-info");
+const log = debug("bobaserver:realms:queries-log");
+const error = debug("bobaserver:realms:queries-error");
 
 const CURSOR_SETTINGS = {
   // image: "https://cur.cursors-4u.net/nature/nat-2/nat120.cur",
@@ -147,26 +147,132 @@ export const getUserPermissionsForRealm = async ({
 }: {
   firebaseId: string;
   realmId: string;
-}): Promise<RealmPermissions[] | null> => {
-  const userPermissionsGroupedByRoles = await pool.manyOrNone(
-    sql.getUserPermissionsForRealm,
-    {
-      user_id: firebaseId,
-      realm_id: realmId,
+}) => {
+  try {
+    const userPermissionsGroupedByRoles = await pool.manyOrNone(
+      sql.getUserPermissionsForRealm,
+      {
+        user_id: firebaseId,
+        realm_id: realmId,
+      }
+    );
+    if (!userPermissionsGroupedByRoles.length) {
+      return;
     }
-  );
-  if (!userPermissionsGroupedByRoles.length) {
-    return;
+    const userRealmPermissionsGroupedByRoles =
+      userPermissionsGroupedByRoles.map((row) => {
+        return extractRealmPermissions(row.permissions);
+      });
+    return userRealmPermissionsGroupedByRoles.reduce(
+      (userRealmPermissions, userRealmPermissionsGroup) => {
+        return userRealmPermissions.concat(userRealmPermissionsGroup);
+      },
+      []
+    );
+  } catch (e) {
+    error(`Error while getting user permissions for the realm.`);
+    error(e);
+    return false;
   }
-  const userRealmPermissionsGroupedByRoles = userPermissionsGroupedByRoles.map(
-    (row) => {
-      return extractRealmPermissions(row.permissions);
-    }
-  );
-  return userRealmPermissionsGroupedByRoles.reduce(
-    (userRealmPermissions, userRealmPermissionsGroup) => {
-      return userRealmPermissions.concat(userRealmPermissionsGroup);
-    },
-    []
-  );
+};
+
+export const createInvite = async (inviteData: {
+  email: string;
+  inviteCode: string;
+  inviterId: number;
+}) => {
+  const query = `
+    INSERT INTO account_invites(nonce, inviter, invitee_email, duration)
+    VALUES (
+      $/invite_code/,
+      $/inviter_id/,
+      $/email/,
+      INTERVAL '1 WEEK')`;
+  try {
+    await pool.none(query, {
+      invite_code: inviteData.inviteCode,
+      inviter_id: inviteData.inviterId,
+      email: inviteData.email,
+    });
+    log(`Generated invite for email ${inviteData.email}.`);
+    return true;
+  } catch (e) {
+    error(`Error while generating invite.`);
+    error(e);
+    return false;
+  }
+};
+
+export const getInviteDetails = async ({
+  nonce,
+}: {
+  nonce: string;
+}): Promise<{
+  email: string;
+  used: boolean;
+  expired: boolean;
+  inviter: number;
+} | null> => {
+  try {
+    const inviteDetails = await pool.one(sql.getInviteDetails, {
+      nonce,
+    });
+    log(`Fetched details for invite ${nonce}:`);
+    log(inviteDetails);
+    return {
+      email: inviteDetails.invitee_email,
+      expired: inviteDetails.expired,
+      used: inviteDetails.used,
+      inviter: inviteDetails.inviter,
+    };
+  } catch (e) {
+    error(`Error while getting invite details.`);
+    error(e);
+    return null;
+  }
+};
+
+export const markInviteUsed = async ({
+  nonce,
+}: {
+  nonce: string;
+}): Promise<boolean> => {
+  const query = `
+    UPDATE account_invites
+    SET used = TRUE
+    WHERE nonce = $/nonce/`;
+  try {
+    await pool.none(query, {
+      nonce,
+    });
+    log(`Marking invite ${nonce} as used.`);
+    return true;
+  } catch (e) {
+    error(`Error while marking invite as used.`);
+    error(e);
+    return false;
+  }
+};
+
+export const createNewUser = async (user: {
+  firebaseId: string;
+  invitedBy: number;
+  createdOn: string;
+}) => {
+  const query = `
+    INSERT INTO users(firebase_id, invited_by, created_on)
+    VALUES ($/firebase_id/, $/invited_by/, $/created_on/)`;
+  try {
+    await pool.none(query, {
+      firebase_id: user.firebaseId,
+      invited_by: user.invitedBy,
+      created_on: user.createdOn,
+    });
+    log(`Added new user in DB for firebase ID ${user.firebaseId}`);
+    return true;
+  } catch (e) {
+    error(`Error creating a new user.`);
+    error(e);
+    return false;
+  }
 };
