a62a4a9bb05da4d9e72ded44dedfee9b216c17c8
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-03-26 07:21:03 -0700

    Fix inviter_id property name agreement

--- server/realms/tests/invites.test.ts
+++ server/realms/tests/invites.test.ts
@@ -168,6 +168,7 @@ describe("Tests get invites endpoint", () => {
       expect(resTwistedMinds.body.invites).toHaveLength(3);
       for (const invite of resTwistedMinds.body.invites) {
         expect(invite.realm_id).toEqual(TWISTED_MINDS_REALM_STRING_ID);
+        expect(invite.inviter_id).toEqual(BOBATAN_USER_ID);
         log(invite.issued_at);
         // I think checking the issued_at is failing because of timezones. Not sure it's worth chasing, looks fine in log
         // expect(Date.parse(invite.issued_at)).toBeLessThan(Date.now());
@@ -201,6 +202,7 @@ describe("Tests get invites endpoint", () => {
       expect(resUwu.body.invites).toHaveLength(2);
       for (const invite of resUwu.body.invites) {
         expect(invite.realm_id).toEqual(UWU_REALM_STRING_ID);
+        expect(invite.inviter_id).toEqual(ZODIAC_KILLER_USER_ID);
         // expect(Date.parse(invite.issued_at)).toBeLessThan(Date.now());
         expect(Date.parse(invite.expires_at)).toBeGreaterThan(Date.now());
         if (invite.invitee_email === UWU_INVITES[0].email) {
