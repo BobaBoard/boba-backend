e5c7b76019bb48adab9b2b4781165e7727d37721
Rebecca Lynde-Scott <rebeccalyndescott@gmail.com>
2022-04-08 19:31:12 -0700

    Added primary keys to some linking tables, caught a typo, added a little spacing for readability, deleted two auditing tables.

--- db/init/000_init.sql
+++ db/init/000_init.sql
@@ -21,9 +21,11 @@ CREATE UNIQUE INDEX users_firebase_id on users(firebase_id);
  */
 CREATE TABLE IF NOT EXISTS friends
 (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     friend_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL
 );
+
 CREATE INDEX friends_user on friends(user_id);
 
 CREATE TABLE IF NOT EXISTS realms
@@ -105,49 +107,23 @@ CREATE INDEX boards_slug on boards(slug);
 
 CREATE TABLE IF NOT EXISTS board_tags
 (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT NOT NULL,
     tag_id BIGINT REFERENCES tags(id) ON DELETE RESTRICT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tag_deny_list
 (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     tag_id BIGINT REFERENCES tags(id) ON DELETE RESTRICT NOT NULL,
     /* If not null the tag is deny-listed only on the given board. */
     board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT
 );
-CREATE INDEX tag_denly_list_user_id on tag_deny_list(user_id);
-
-CREATE TABLE IF NOT EXISTS board_watchers (
-    board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT NOT NULL,
-    user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
-    /* UTC timestamp. */
-    last_access timestamp NOT NULL DEFAULT now(),
-    notifications_enabled BOOLEAN NOT NULL DEFAULT FALSE
-);
-CREATE INDEX board_watchers_board on board_watchers(board_id);
-CREATE INDEX board_watchers_user_id on board_watchers(user_id);
-
-CREATE TABLE IF NOT EXISTS collections
-(
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-    string_id TEXT NOT NULL,
-    title TEXT NOT NULL,
-    content_description TEXT NOT NULL,
-    /**
-     * Whisper Tags are textual tags that do not get indicized but act as an extra
-     * space for comments.
-     */
-    whisper_tags TEXT[]
-);
-CREATE UNIQUE INDEX collections_string_id on collections(string_id);
-
-CREATE TABLE IF NOT EXISTS collection_tags (
-    collection_id BIGINT REFERENCES collections(id) ON DELETE RESTRICT NOT NULL,
-    tag_id BIGINT REFERENCES tags(id) ON DELETE RESTRICT NOT NULL
-);
+CREATE INDEX tag_deny_list_user_id on tag_deny_list(user_id);
 
 CREATE TYPE view_types AS ENUM ('thread', 'gallery', 'timeline');
+
 CREATE TABLE IF NOT EXISTS threads
 (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
@@ -158,21 +134,12 @@ CREATE TABLE IF NOT EXISTS threads
 );
 CREATE INDEX threads_string_id on threads(string_id);
 
-CREATE TABLE IF NOT EXISTS thread_watchers (
-    thread_id BIGINT REFERENCES threads(id) ON DELETE RESTRICT NOT NULL,
-    user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
-    /* UTC timestamp. */
-    last_access timestamp NOT NULL DEFAULT now(),
-    notifications_enabled BOOLEAN NOT NULL DEFAULT FALSE
-);
-CREATE INDEX thread_watchers_thread_id on thread_watchers(thread_id);
-CREATE INDEX thread_watchers_user_id on thread_watchers(user_id);
-
 /* TODO: decide whether to switch this to who the user is visible to rather than hidden from. */
 CREATE TYPE anonymity_type AS ENUM ('everyone', 'strangers');
 CREATE TYPE post_type AS ENUM ('text');
 
-CREATE TABLE IF NOT EXISTS posts (
+CREATE TABLE IF NOT EXISTS posts 
+(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     string_id TEXT NOT NULL,
     parent_thread BIGINT REFERENCES threads(id) ON DELETE RESTRICT NOT NULL,
@@ -196,35 +163,35 @@ CREATE INDEX posts_string_id on posts(string_id);
 CREATE INDEX posts_parent_thread on posts(parent_thread);
 CREATE INDEX posts_author on posts(author);
 
-CREATE TABLE IF NOT EXISTS post_tags (
+CREATE TABLE IF NOT EXISTS post_tags 
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     post_id BIGINT REFERENCES posts(id) ON DELETE RESTRICT NOT NULL,
     tag_id BIGINT REFERENCES tags(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE INDEX post_tags_post_id on post_tags(post_id);
 CREATE INDEX post_tags_tag_id on post_tags(tag_id);
 
-CREATE TABLE IF NOT EXISTS post_categories (
+CREATE TABLE IF NOT EXISTS post_categories 
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     post_id BIGINT REFERENCES posts(id) ON DELETE RESTRICT NOT NULL,
     category_id BIGINT REFERENCES categories(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE INDEX post_categories_post_id on post_categories(post_id);
 CREATE INDEX post_categories_category_id on post_categories(category_id);
 
-CREATE TABLE IF NOT EXISTS post_warnings (
+CREATE TABLE IF NOT EXISTS post_warnings 
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     post_id BIGINT REFERENCES posts(id) ON DELETE RESTRICT NOT NULL,
     warning_id BIGINT REFERENCES content_warnings(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE INDEX post_warnings_post_id on post_warnings(post_id);
 CREATE INDEX post_warnings_category_id on post_warnings(warning_id);
 
-CREATE TABLE IF NOT EXISTS post_audits (
-    post_id BIGINT REFERENCES posts(id) ON DELETE RESTRICT NOT NULL,
-    deleted_by BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
-    /* UTC timestamp. */
-    deleted_time timestamp NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS comments (
+CREATE TABLE IF NOT EXISTS comments 
+(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     string_id TEXT NOT NULL,
     parent_thread BIGINT REFERENCES threads(id) ON DELETE RESTRICT NOT NULL,
@@ -246,33 +213,33 @@ CREATE INDEX comments_parent_thread on comments(parent_thread);
 CREATE INDEX comments_parent_post on comments(parent_post);
 CREATE INDEX comments_author on comments(author);
 
-CREATE TABLE IF NOT EXISTS comment_audits (
-    comment_id BIGINT REFERENCES comments(id) ON DELETE RESTRICT NOT NULL,
-    deleted_by BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
-    /* UTC timestamp. */
-    deleted_time timestamp NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS user_board_last_visits(
+CREATE TABLE IF NOT EXISTS user_board_last_visits
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT NOT NULL,
     last_visit_time timestamp NOT NULL DEFAULT now()
 );
 CREATE UNIQUE INDEX user_board_entry on user_board_last_visits(user_id, board_id);
 
-CREATE TABLE IF NOT EXISTS user_thread_last_visits(
+CREATE TABLE IF NOT EXISTS user_thread_last_visits
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     thread_id BIGINT REFERENCES threads(id) ON DELETE RESTRICT NOT NULL,
     last_visit_time timestamp NOT NULL DEFAULT now()
 );
 CREATE UNIQUE INDEX user_thread_entry on user_thread_last_visits(user_id, thread_id);
 
-CREATE TABLE IF NOT EXISTS dismiss_notifications_requests(
+CREATE TABLE IF NOT EXISTS dismiss_notifications_requests
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     realm_id BIGINT REFERENCES realms(id) ON DELETE RESTRICT NOT NULL,
     dismiss_request_time timestamp NOT NULL DEFAULT now()
 );
 CREATE UNIQUE INDEX dismiss_notifications_requests_entry on dismiss_notifications_requests(user_id, realm_id);
+
 
 CREATE TABLE IF NOT EXISTS dismiss_board_notifications_requests(
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
@@ -281,39 +248,49 @@ CREATE TABLE IF NOT EXISTS dismiss_board_notifications_requests(
 );
 CREATE UNIQUE INDEX dismiss_board_notifications_requests_entry on dismiss_board_notifications_requests(user_id, board_id);
 
-CREATE TABLE IF NOT EXISTS user_muted_threads(
+CREATE TABLE IF NOT EXISTS user_muted_threads
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     thread_id BIGINT REFERENCES threads(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE UNIQUE INDEX user_muted_thread_entry on user_muted_threads(user_id, thread_id);
 
-CREATE TABLE IF NOT EXISTS user_muted_boards(
+CREATE TABLE IF NOT EXISTS user_muted_boards
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE UNIQUE INDEX user_muted_boards_entry on user_muted_boards(user_id, board_id);
 
-CREATE TABLE IF NOT EXISTS user_starred_threads(
+CREATE TABLE IF NOT EXISTS user_starred_threads
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     thread_id BIGINT REFERENCES threads(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE UNIQUE INDEX user_starred_thread_entry on user_starred_threads(user_id, thread_id);
 
-CREATE TABLE IF NOT EXISTS user_pinned_boards(
+CREATE TABLE IF NOT EXISTS user_pinned_boards
+(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE UNIQUE INDEX user_pinned_boards_entry on user_pinned_boards(user_id, board_id);
 
-CREATE TABLE IF NOT EXISTS user_hidden_threads(
+CREATE TABLE IF NOT EXISTS user_hidden_threads
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     user_id BIGINT REFERENCES users(id) ON DELETE RESTRICT NOT NULL,
     thread_id BIGINT REFERENCES threads(id) ON DELETE RESTRICT NOT NULL
 );
 CREATE UNIQUE INDEX user_hidden_thread_entry on user_hidden_threads(user_id, thread_id);
 
 CREATE TYPE board_description_section_type AS ENUM ('text', 'category_filter');
-CREATE TABLE IF NOT EXISTS board_description_sections(
+CREATE TABLE IF NOT EXISTS board_description_sections
+(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     string_id TEXT NOT NULL,
     board_id BIGINT REFERENCES boards(id) ON DELETE RESTRICT NOT NULL,
@@ -325,7 +302,9 @@ CREATE TABLE IF NOT EXISTS board_description_sections(
 CREATE INDEX board_description_sections_board_id on board_description_sections(board_id);
 CREATE UNIQUE INDEX board_description_sections_string_id on board_description_sections(string_id);
 
-CREATE TABLE IF NOT EXISTS board_description_section_categories(
+CREATE TABLE IF NOT EXISTS board_description_section_categories
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     section_id BIGINT REFERENCES board_description_sections(id) ON DELETE RESTRICT NOT NULL,
     category_id BIGINT REFERENCES categories(id) ON DELETE RESTRICT NOT NULL
 );
