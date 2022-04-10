c060bfc4782ce7cbf67e9b59f196e56ef55a198e
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2022-04-10 02:09:04 -0700

    Try to pass open-api-validate

--- server/realms/routes.ts
+++ server/realms/routes.ts
@@ -311,7 +311,7 @@ router.delete("/:realm_id/notifications", ensureLoggedIn, async (req, res) => {
  * /realms/{realm_id}/invites:
  *   get:
  *     summary: List all pending invites for the realm
- *     description: See https://github.com/essential-randomness/bobaserver/issues/56 for future design intentions to return all invites.
+ *     # description: See https://github.com/essential-randomness/bobaserver/issues/56 for future design intentions to return all invites.
  *     operationId: getInvitesByRealmId
  *     tags:
  *       - /realms/
