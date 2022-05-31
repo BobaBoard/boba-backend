9241384a92748db5d46de196b79c1cd2fdfb9471
Essential Randomness <essential.randomn3ss@gmail.com>
2022-05-31 11:29:44 -0700

    move thread creation subscription event check to an events file

---
+++ server/subscriptions/events.ts
@@ -0,0 +1,39 @@
+import * as threadEvents from "handlers/events/threads";
+
+import { CacheKeys, cache } from "server/cache";
+
+import axios from "axios";
+import { getTriggeredWebhooks } from "server/threads/queries";
+
+threadEvents.register(
+  threadEvents.EVENT_TYPES.THREAD_CREATED,
+  async ({ thread }) => {
+    const webhooks = await getTriggeredWebhooks({
+      slug: thread.parent_board_slug,
+      categories: thread.posts[0].tags?.category_tags,
+    });
+    if (webhooks && webhooks.length > 0) {
+      // TODO: add parent realm slug to thread
+      const threadUrl = `https://v0.boba.social/!${thread.parent_board_slug}/thread/${thread.id}`;
+      webhooks.forEach(
+        async ({ webhook, subscriptionNames, subscriptionIds }) => {
+          await Promise.all(
+            subscriptionIds.map((subscriptionId) =>
+              cache().hdel(CacheKeys.SUBSCRIPTION, subscriptionId)
+            )
+          );
+          const message = `Your "${subscriptionNames.join(
+            ", "
+          )}" subscription has updated!\n${threadUrl}`;
+          axios.post(webhook, {
+            content: message,
+            username: thread.posts[0].secret_identity.name,
+            avatar_url: thread.posts[0].secret_identity.avatar,
+          });
+        }
+      );
+    }
+  }
+);
+
+threadEvents.register(threadEvents.EVENT_TYPES.THREAD_UPDATED, () => {});
