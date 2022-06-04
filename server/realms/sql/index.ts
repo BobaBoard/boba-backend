a82cf911bff66bedb337dc4f84a0c1316605d27e
Essential Randomness <essential.randomn3ss@gmail.com>
2022-06-04 16:33:29 -0700

    return homepage blocks as part of /realms/slug endpoint

--- server/realms/sql/index.ts
+++ server/realms/sql/index.ts
@@ -1,5 +1,30 @@
 const getRealmBySlug = `
-    SELECT * FROM realms WHERE slug = $/realm_slug/`;
+    SELECT 
+      realms.string_id AS realm_id,
+      realms.slug AS realm_slug,
+      array_to_json(
+        array_agg(block_with_rules)) AS homepage_blocks
+    FROM realms 
+      JOIN realm_homepage_blocks AS rhb
+        ON realms.id = rhb.realm_id 
+      JOIN (
+        SELECT 
+          blocks.id,
+          blocks.string_id,
+          blocks.title,
+          blocks.index,
+          blocks.type,
+          array_to_json(array_agg(rules)) AS rules
+        FROM blocks
+          JOIN block_rules AS br 
+            ON br.block_id = blocks.id 
+          JOIN rules
+            ON br.rule_id = rules.id
+        GROUP BY blocks.id, blocks.string_id, blocks.title, blocks.index, blocks.type
+      ) AS block_with_rules
+        ON rhb.block_id = block_with_rules.id
+    WHERE slug = $/realm_slug/
+    GROUP BY realms.string_id, realms.slug;`;
 
 const dismissNotifications = `
     INSERT INTO dismiss_notifications_requests(user_id, realm_id, dismiss_request_time) VALUES (
