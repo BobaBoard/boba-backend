5ef643744b7e369337799fe172b780bc799fc301
Essential Randomness <essential.randomn3ss@gmail.com>
2023-02-11 00:05:30 -0800

    realm by slug db query got zodded

---
+++ server/realms/sql/types.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const GetRealmBySlugDbSchema = z.object({
+  realm_id: z.string(),
+  realm_slug: z.string(),
+  realm_icon_url: z.string(),
+  realm_favicon_url: z.string().nullable(),
+  realm_title: z.string().nullable(),
+  realm_description: z.string().nullable(),
+  realm_feedback_form_url: z.string().nullable(),
+  homepage_blocks: z.any(),
+});
+
+export type GetRealmBySlugDbType = z.infer<typeof GetRealmBySlugDbSchema>;
