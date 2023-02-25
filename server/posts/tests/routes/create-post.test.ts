63f0abf7b89f5672fffe47690f109eee1cd7a4e2
spiney <mindthespines@pm.me>
2023-02-25 10:37:49 -0800

    sketch out comment tests

--- server/posts/tests/routes/create-post.test.ts
+++ server/posts/tests/routes/create-post.test.ts
@@ -1,6 +1,7 @@
 import * as uuid from "uuid";
 
 import { BOBATAN_USER_ID, ZODIAC_KILLER_USER_ID } from "test/data/auth";
+import { Comment, Post } from "types/rest/threads";
 import {
   EVENT_TYPES as THREAD_EVENT_TYPES,
   ThreadUpdatedPayload,
@@ -15,7 +16,6 @@ import {
 import { CHARACTER_TO_MAIM_POST_ID } from "test/data/posts";
 import { EventEmitter } from "events";
 import { FAVORITE_CHARACTER_THREAD_ID } from "test/data/threads";
-import { Post } from "types/rest/threads";
 import debug from "debug";
 import { mocked } from "jest-mock";
 import request from "supertest";
@@ -138,6 +138,115 @@ describe("Test creating new post REST API", () => {
         boardSlug: "gore",
         post: expectedResponse,
       });
+    });
+  });
+});
+
+describe("Test commenting on post REST API", () => {
+  const server = startTestServer(router);
+
+  const testCommentBody: {
+    contents: Comment[];
+    reply_to_comment_id: string | null;
+    accessory_id: string | null;
+    identity_id: string | null;
+  } = {
+    contents: [
+      {
+        id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
+        parent_post_id: CHARACTER_TO_MAIM_POST_ID,
+        parent_comment_id: null,
+        chain_parent_id: null,
+        content: "HEY I HAVE SOMETHING TO SAY",
+        secret_identity: {
+          name: "Seeeeeekrit Identity",
+          avatar:
+            "https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fgore%2F5c2c3867-2323-4209-8bd4-9dfcc88808f3%2Fd931f284-5c22-422d-9343-e509cfb44ffc.png?alt=media&token=94e52fff-4e6b-4110-94c3-90b8800f541c",
+          color: null,
+          accessory: null,
+        },
+        user_identity: {
+          name: "realUser",
+          avatar:
+            "https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fgore%2F5c2c3867-2323-4209-8bd4-9dfcc88808f3%2Fd931f284-5c22-422d-9343-e509cfb44ffc.png?alt=media&token=94e52fff-4e6b-4110-94c3-90b8800f541c",
+        },
+        created_at: "2019-08-24T14:15:22Z",
+        own: true,
+        new: true,
+        friend: true,
+      },
+    ],
+    reply_to_comment_id: null,
+    accessory_id: null,
+    identity_id: null,
+  };
+
+  test("doesn't allow commenting on a post when logged out", async () => {
+    await wrapWithTransaction(async () => {
+      const res = await request(server.app)
+        .post(`/${CHARACTER_TO_MAIM_POST_ID}/comments`)
+        .send(testCommentBody);
+
+      expect(res.status).toBe(401);
+      expect(res.body).toEqual({
+        message: "No authenticated user found.",
+      });
+    });
+  });
+
+  test("doesn't allow commenting on a post when user not a member of realm", async () => {
+    await wrapWithTransaction(async () => {
+      setLoggedInUser(ZODIAC_KILLER_USER_ID);
+      const res = await request(server.app)
+        .post(`/${CHARACTER_TO_MAIM_POST_ID}/comments`)
+        .send(testCommentBody);
+
+      expect(res.status).toBe(403);
+      expect(res.body).toEqual({
+        message: "User does not have required permissions for realm operation.",
+      });
+    });
+  });
+
+  test("allows commenting on a post when logged in", async () => {
+    await wrapWithTransaction(async () => {
+      setLoggedInUser(BOBATAN_USER_ID);
+      const commentId = testCommentBody.contents[0].id;
+      jest.spyOn(uuid, "v4").mockReturnValueOnce(commentId);
+      const mockedEmit = jest.spyOn(EventEmitter.prototype, "emit");
+      const res = await request(server.app)
+        .post(`/${CHARACTER_TO_MAIM_POST_ID}/comments`)
+        .send(testCommentBody);
+
+        // TODO: see if this actually looks right
+      const expectedResponse = {
+        comments: [
+          {
+            id: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
+            parent_comment_id: null,
+            chain_parent_id: null,
+            parent_post_id: "11b85dac-e122-40e0-b09a-8829c5e0250e",
+            created_at: expect.any(String),
+            content:
+              '{"id":"497f6eca-6276-4993-bfeb-53cbbbba6f08","parent_post_id":"11b85dac-e122-40e0-b09a-8829c5e0250e","parent_comment_id":null,"chain_parent_id":null,"content":"HEY I HAVE SOMETHING TO SAY","secret_identity":{"name":"Seeeeeekrit Identity","avatar":"https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fgore%2F5c2c3867-2323-4209-8bd4-9dfcc88808f3%2Fd931f284-5c22-422d-9343-e509cfb44ffc.png?alt=media&token=94e52fff-4e6b-4110-94c3-90b8800f541c","color":null,"accessory":null},"user_identity":{"name":"realUser","avatar":"https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fgore%2F5c2c3867-2323-4209-8bd4-9dfcc88808f3%2Fd931f284-5c22-422d-9343-e509cfb44ffc.png?alt=media&token=94e52fff-4e6b-4110-94c3-90b8800f541c"},"created_at":"2019-08-24T14:15:22Z","own":true,"new":true,"friend":true}',
+            secret_identity: {
+              name: "Old Time-y Anon",
+              avatar:
+                "https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fgore%2F5c2c3867-2323-4209-8bd4-9dfcc88808f3%2Fd931f284-5c22-422d-9343-e509cfb44ffc.png?alt=media&token=94e52fff-4e6b-4110-94c3-90b8800f541c",
+              color: null,
+              accessory:
+                "https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fbobaland%2Fundefined%2F9b7a5d90-4885-43bf-a5f5-e861b7b87505.png?alt=media&token=83ae88ca-5c81-4d1b-9208-0a936017c485",
+            },
+            user_identity: { name: "bobatan", avatar: "/bobatan.png" },
+            friend: false,
+            own: true,
+            new: true,
+          },
+        ],
+      };
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual(expectedResponse);
     });
   });
 });
