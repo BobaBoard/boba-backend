f36509ffe548e3d0cee2a9189bbc547b8e36c6d8
spiney <mindthespines@pm.me>
2023-03-05 09:00:30 -0800

    add patch user settings test

--- server/users/tests/routes.test.ts
+++ server/users/tests/routes.test.ts
@@ -91,15 +91,20 @@ describe("Tests basic routes of users REST API", () => {
   });
 
   describe("PATCH tests", () => {
-    const testPatch = {
+    const testUserPatch = {
       username: "SnazzyNewName",
       avatarUrl:
         "https://firebasestorage.googleapis.com/v0/b/bobaboard-fb.appspot.com/o/images%2Fusers%2Favatar%2F4f5fde75-e670-4bb2-a04f-e90a5038ee34.jpeg?alt=media&token=7bdc5534-3159-4856-8d98-937cc1ee642f",
     };
 
+    const testSettingsPatch = {
+      name: "FESTIVE_BACKGROUND_HEADER",
+      value: false,
+    };
+
     test("prevents unauthorized PATCH access to the @me endpoint", async () => {
       await wrapWithTransaction(async () => {
-        const res = await request(server.app).patch(`/@me`).send(testPatch);
+        const res = await request(server.app).patch(`/@me`).send(testUserPatch);
 
         expect(res.status).toBe(401);
       });
@@ -109,12 +114,12 @@ describe("Tests basic routes of users REST API", () => {
       await wrapWithTransaction(async () => {
         setLoggedInUser(JERSEY_DEVIL_USER_ID);
 
-        const res = await request(server.app).patch(`/@me`).send(testPatch);
+        const res = await request(server.app).patch(`/@me`).send(testUserPatch);
 
         expect(res.status).toBe(200);
         expect(res.body).toEqual({
-          username: testPatch.username,
-          avatar_url: testPatch.avatarUrl,
+          username: testUserPatch.username,
+          avatar_url: testUserPatch.avatarUrl,
         });
       });
     });
@@ -135,10 +140,40 @@ describe("Tests basic routes of users REST API", () => {
       await wrapWithTransaction(async () => {
         setLoggedInUser(JERSEY_DEVIL_USER_ID);
         jest.spyOn(userQueries, "updateUserData").mockResolvedValueOnce(null);
-        const res = await request(server.app).patch(`/@me`).send(testPatch);
+        const res = await request(server.app).patch(`/@me`).send(testUserPatch);
 
         expect(res.status).toBe(500);
         expect(res.body).toEqual({});
+      });
+    });
+
+    test("prevents unauthorized PATCH access to the @me settings endpoint", async () => {
+      await wrapWithTransaction(async () => {
+        const res = await request(server.app)
+          .patch(`/@me/settings`)
+          .send(testSettingsPatch);
+
+        expect(res.status).toBe(401);
+      });
+    });
+
+    test("updates user settings", async () => {
+      await wrapWithTransaction(async () => {
+        setLoggedInUser(JERSEY_DEVIL_USER_ID);
+        const res = await request(server.app)
+          .patch(`/@me/settings`)
+          .send(testSettingsPatch);
+
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual({
+          decorations: [
+            {
+              name: "FESTIVE_BACKGROUND_HEADER",
+              type: "BOOLEAN",
+              value: false,
+            },
+          ],
+        });
       });
     });
   });
