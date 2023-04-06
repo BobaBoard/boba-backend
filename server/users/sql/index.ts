4094352331064cbeb01756d42b6d4e92838bc22e
spiney <mindthespines@pm.me>
2023-04-05 17:22:52 -0700

    flesh out fuller query and add tests

--- server/users/sql/index.ts
+++ server/users/sql/index.ts
@@ -42,9 +42,14 @@ VALUES ($/firebase_id/, $/invited_by/, $/created_on/)`;
 const getAllUserRoles = `
   SELECT
     roles.*,
-    realms.string_id AS realm_external_id,
-    boards.string_id AS board_external_id
+    COALESCE(ARRAY_AGG(DISTINCT realms.string_id) FILTER (WHERE realms.string_id IS NOT NULL), '{}') realm_ids,
+    COALESCE(ARRAY_AGG(DISTINCT boards.string_id) FILTER (WHERE boards.string_id IS NOT NULL), '{}') board_ids,
+    accessories.string_id AS accessory_external_id
   FROM roles
+  LEFT JOIN role_accessories ON
+  	roles.id = role_accessories.role_id
+  LEFT JOIN accessories ON
+  	accessories.id = role_accessories.accessory_id
   LEFT JOIN realm_user_roles ON
     roles.id = realm_user_roles.role_id
   LEFT JOIN board_user_roles ON
@@ -55,9 +60,8 @@ const getAllUserRoles = `
     realms.id = realm_user_roles.realm_id
   LEFT JOIN boards ON
     boards.id = board_user_roles.board_id
-  WHERE users.firebase_id = $/firebase_id/`;
-// should work now, not clear if the user join is a possible point of weirdness, could be a problem if we have, like, roles that are associated with a board but not a realm? which isn't true in the test data, but should be wary here
-// what we'll want to do here is aggregate down duplicates (which don't exist in the test data right now, so we may want to make some examples to test on) - the same role on multiple boards should only appear once but the boards field should be an array with the list of boards where it appears; same for realms
+  WHERE users.firebase_id = $/firebase_id/
+  GROUP BY roles.id, accessory_external_id`;
 
 const getUserRolesByRealm = `
   SELECT
