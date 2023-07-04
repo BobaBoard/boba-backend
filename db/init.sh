eed4913af09f9276d346fa53942f95ae75d995e6
Essential Randomness <essential.randomn3ss@gmail.com>
2023-07-04 13:28:51 -0700

    allow passing argument to db init function

--- db/init.sh
+++ db/init.sh
@@ -1,24 +1,32 @@
+#!/bin/bash
 set -e
+
+DB_INIT_DIR=${1:-docker-entrypoint-initdb.d}
+
 echo "****************"
 echo "****************"
 echo "Running db init!"
 echo "****************"
 echo "****************"
-for f in docker-entrypoint-initdb.d/init/*.sql; do 
+
+for f in $DB_INIT_DIR/init/*.sql; do 
   echo "$0: running $f"
   psql -U $POSTGRES_USER $POSTGRES_DB -f $f -v ON_ERROR_STOP=1
   echo $?
 done
+
 echo "****************"
 echo "****************"
 echo "Done with db creation!"
 echo "****************"
 echo "****************"
-for f in docker-entrypoint-initdb.d/test_db_init/*.sql; do 
+
+for f in $DB_INIT_DIR/test_db_init/*.sql; do 
   echo "$0: running $f"
   psql -U $POSTGRES_USER $POSTGRES_DB -f $f -v ON_ERROR_STOP=1
   echo $?
 done
+
 echo "****************"
 echo "****************"
 echo "Done with db init!"
