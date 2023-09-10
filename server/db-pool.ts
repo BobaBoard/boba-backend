8b863296336f43f8c3338a4864908b8bd98fc041
Essential Randomness <essential.randomn3ss@gmail.com>
2023-09-10 13:38:25 -0700

    add timesouts to db

--- server/db-pool.ts
+++ server/db-pool.ts
@@ -5,20 +5,26 @@ const error = debug("bobaserver:pool-error");
 const log = debug("bobaserver:pool-log");
 const info = debug("bobaserver:pool-info");
 
-let databaseConfig;
+let databaseConfig = {};
 log(`Attempting db connection...`);
 if (process.env.NODE_ENV == "production") {
   log(`Connecting to remote database host: ${process.env.POSTGRES_HOST}`);
   databaseConfig = {
+    ...databaseConfig,
     host: process.env.POSTGRES_HOST,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    connectionTimeoutMillis: 3000,
+    query_timeout: 3000,
   };
 } else {
   log(`Attempting connection to local db on port ${process.env.POSTGRES_PORT}`);
   const DATABASE_URL = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@127.0.0.1:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-  databaseConfig = { connectionString: DATABASE_URL };
+  databaseConfig = {
+    ...databaseConfig,
+    connectionString: DATABASE_URL,
+  };
 }
 const pgLib = pgp({
   connect: () => {
