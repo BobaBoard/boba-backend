0d76fa1f831e47fe9c17f88fe75fddef6411db38
Essential Randomness <essential.randomn3ss@gmail.com>
2023-09-19 18:28:23 -0700

    initial cleanup
    also generate yaml spec
    
    fix some more feed types
    
    add parameters to user feed
    
    add read me
    
    fix accidental change override
    
    move openapi configs to a different folder
    
    move openapi generation to the correct folders
    
    add todo for unzodded queries

--- server/feeds/queries.ts
+++ server/feeds/queries.ts
@@ -1,13 +1,12 @@
+import { FeedTypeSchema, ZodDbFeedType } from "zodtypes";
 import { decodeCursor, encodeCursor } from "utils/queries-utils";
 
-import { ZodDbFeedType } from "types/db/schemas";
 import debug from "debug";
 import pool from "server/db-pool";
 import sql from "./sql";
 
 const info = debug("bobaserver:feeds:queries-info");
 const log = debug("bobaserver:feeds:queries-log");
-const error = debug("bobaserver:feeds:queries-error");
 
 const DEFAULT_PAGE_SIZE = 10;
 
@@ -58,6 +57,7 @@ export const getRealmActivityByExternalId = async ({
   }
 
   log(`Fetched realm ${realmExternalId} activity data for user ${firebaseId}`);
+  // TODO[zod]: Zod this
   return { cursor: nextCursor, activity: rows };
 };
 
@@ -74,52 +74,45 @@ export const getBoardActivityByExternalId = async ({
   cursor: string | null;
   pageSize?: number;
 }): Promise<ZodDbFeedType | null | false> => {
-  try {
-    const decodedCursor = cursor ? decodeCursor(cursor) : null;
+  const decodedCursor = cursor ? decodeCursor(cursor) : null;
+  const finalPageSize =
+    decodedCursor?.page_size || pageSize || DEFAULT_PAGE_SIZE;
 
-    const finalPageSize =
-      decodedCursor?.page_size || pageSize || DEFAULT_PAGE_SIZE;
-    const rows = await pool.manyOrNone(sql.getBoardActivityByExternalId, {
-      board_id: boardExternalId,
-      firebase_id: firebaseId,
-      filtered_category: categoryFilter || null,
-      last_activity_cursor: decodedCursor?.last_activity_cursor || null,
+  const rows = await pool.manyOrNone(sql.getBoardActivityByExternalId, {
+    board_id: boardExternalId,
+    firebase_id: firebaseId,
+    filtered_category: categoryFilter || null,
+    last_activity_cursor: decodedCursor?.last_activity_cursor || null,
+    page_size: finalPageSize,
+  });
+
+  if (!rows) {
+    log(`Board not found: ${boardExternalId}`);
+    return null;
+  }
+
+  if (rows.length == 1 && rows[0].thread_id == null) {
+    // Only one row with just the null thread)
+    log(`Board empty: ${boardExternalId}`);
+    return { cursor: null, activity: [] };
+  }
+
+  let result = rows;
+  let nextCursor = null;
+  info(`Got getBoardActivityByExternalId query result`, result);
+  if (result.length > finalPageSize) {
+    nextCursor = encodeCursor({
+      last_activity_cursor:
+        result[result.length - 1].thread_last_activity_at_micro,
       page_size: finalPageSize,
     });
-
-    if (!rows) {
-      log(`Board not found: ${boardExternalId}`);
-      return null;
-    }
-
-    if (rows.length == 1 && rows[0].thread_id == null) {
-      // Only one row with just the null thread)
-      log(`Board empty: ${boardExternalId}`);
-      return { cursor: null, activity: [] };
-    }
-
-    let result = rows;
-    let nextCursor = null;
-    info(`Got getBoardActivityByExternalId query result`, result);
-    if (result.length > finalPageSize) {
-      nextCursor = encodeCursor({
-        last_activity_cursor:
-          result[result.length - 1].thread_last_activity_at_micro,
-        page_size: finalPageSize,
-      });
-      // remove last element from array
-      result.pop();
-    }
-
-    log(
-      `Fetched board ${boardExternalId} activity data for user ${firebaseId}`
-    );
-    return { cursor: nextCursor, activity: rows };
-  } catch (e) {
-    error(`Error while fetching board by slug (${boardExternalId}).`);
-    error(e);
-    return false;
+    // remove last element from array
+    result.pop();
   }
+
+  log(`Fetched board ${boardExternalId} activity data for user ${firebaseId}`);
+  // TODO[zod]: Zod this
+  return { cursor: nextCursor, activity: rows };
 };
 
 export const getUserActivity = async ({
@@ -137,45 +130,39 @@ export const getUserActivity = async ({
   cursor: string | null;
   pageSize?: number;
 }): Promise<ZodDbFeedType | false> => {
-  try {
-    const decodedCursor = cursor ? decodeCursor(cursor) : null;
+  const decodedCursor = cursor ? decodeCursor(cursor) : null;
 
-    const finalPageSize =
-      decodedCursor?.page_size || pageSize || DEFAULT_PAGE_SIZE;
-    const rows = await pool.manyOrNone(sql.getUserFeedActivity, {
-      firebase_id: firebaseId,
-      last_activity_cursor: decodedCursor?.last_activity_cursor || null,
-      page_size: finalPageSize,
-      updated_only: updatedOnly,
-      own_only: ownOnly,
-      realm_id: realmExternalId,
-    });
+  const finalPageSize =
+    decodedCursor?.page_size || pageSize || DEFAULT_PAGE_SIZE;
+  const rows = await pool.manyOrNone(sql.getUserFeedActivity, {
+    firebase_id: firebaseId,
+    last_activity_cursor: decodedCursor?.last_activity_cursor || null,
+    page_size: finalPageSize,
+    updated_only: updatedOnly,
+    own_only: ownOnly,
+    realm_id: realmExternalId,
+  });
 
-    if (rows.length == 1 && rows[0].thread_id == null) {
-      // Only one row with just the null thread)
-      log(`Feed empty.`);
-      return { cursor: null, activity: [] };
-    }
-
-    let result = rows;
-    let nextCursor = null;
-    info(`Got getUserActivity query result`, result);
-    if (result.length > finalPageSize) {
-      nextCursor = encodeCursor({
-        last_activity_cursor:
-          result[result.length - 1].thread_last_activity_at_micro,
-        page_size: finalPageSize,
-      });
-      // remove last element from array
-      result.pop();
-    }
-
-    return { cursor: nextCursor, activity: rows };
-  } catch (e) {
-    error(`Error while fetching activity for user (${firebaseId}).`);
-    error(e);
-    return false;
+  if (rows.length == 1 && rows[0].thread_id == null) {
+    // Only one row with just the null thread)
+    log(`Feed empty.`);
+    return { cursor: null, activity: [] };
   }
+
+  let result = rows;
+  let nextCursor = null;
+  info(`Got getUserActivity query result`, result);
+  if (result.length > finalPageSize) {
+    nextCursor = encodeCursor({
+      last_activity_cursor:
+        result[result.length - 1].thread_last_activity_at_micro,
+      page_size: finalPageSize,
+    });
+    // remove last element from array
+    result.pop();
+  }
+
+  return FeedTypeSchema.parse({ cursor: nextCursor, activity: rows });
 };
 
 export const getUserStarFeed = async ({
@@ -215,5 +202,6 @@ export const getUserStarFeed = async ({
     result.pop();
   }
 
+  // TODO[zod]: Zod this
   return { cursor: nextCursor, activity: rows };
 };
--- utils/generate-spec-file.ts
+++
@@ -1,6 +0,0 @@
-import { specs } from "handlers/open-api";
-import { writeFileSync } from "fs";
-
-writeFileSync("open-api-spec.json", JSON.stringify(specs, null, 2), {
-  flag: "w+",
-});
