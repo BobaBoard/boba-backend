dcef776ce64b3d4ed4d82c7531326b22e09e9eb3
Essential Randomness <essential.randomn3ss@gmail.com>
2023-10-30 17:11:21 -0700

    fix invalid uuid in muted boards

--- db/test_db_init/040_muting_insert.sql
+++ db/test_db_init/040_muting_insert.sql
@@ -2,13 +2,13 @@ WITH
   new_thread_id AS
     (INSERT INTO threads(string_id, parent_board)
       VALUES (
-        'b86710a8-66h7-4c09-b3a5-54668bab7051',
+        '09785026-86fa-44a2-a0b5-22e6c59f01d8',
         (SELECT id FROM boards WHERE slug = 'muted'))
      RETURNING id),
   posts_insert AS 
     (INSERT INTO posts(string_id, parent_post, parent_thread, author, content, type, whisper_tags, anonymity_type, created)
       VALUES
-        ('123f795b-d60d-4016-nn56-8684411f7785',
+        ('4ff5460f-da1a-4de4-8bcd-79c79e2f899b',
          NULL,
          (SELECT id FROM new_thread_id),
          (SELECT id FROM Users WHERE username = 'jersey_devil_69'),
