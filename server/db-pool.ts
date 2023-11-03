68dbdc9c8e4696dee2216b92027c22c7cb7d20ab
Essential Randomness <essential.randomn3ss@gmail.com>
2023-11-03 13:40:20 -0700

    attempt to remove db timeout

--- server/db-pool.ts
+++ server/db-pool.ts
@@ -17,8 +17,8 @@ if (process.env.NODE_ENV == "production") {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     port: process.env.POSTGRES_PORT,
-    connectionTimeoutMillis: 3000,
-    query_timeout: 3000,
+    // connectionTimeoutMillis: 3000,
+    // query_timeout: 3000,
   };
   if (process.env.POSTGRES_SSL_ROOT_CERT_PATH) {
     databaseConfig.ssl = {
